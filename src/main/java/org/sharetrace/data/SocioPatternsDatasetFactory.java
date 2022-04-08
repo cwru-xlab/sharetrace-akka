2a6739d2d8a2ee612a8ed444b8699b1d2e794371
rtatton <rdt17@protonmail.com>
2022-04-07 22:50:45 -0400

    Calibrate timestamps for SocioPatterns
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/data/SocioPatternsDatasetFactory.java
+++ src/main/java/org/sharetrace/data/SocioPatternsDatasetFactory.java
@@ -11,25 +11,37 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
+import org.immutables.builder.Builder;
 import org.jgrapht.Graph;
 import org.sharetrace.model.graph.Edge;
 import org.sharetrace.model.message.RiskScore;
 
 public class SocioPatternsDatasetFactory extends DatasetFactory {
 
-  private static final String DEFAULT_DELIMITER = ",";
   private final Map<Set<Integer>, Instant> contacts;
-  private final Supplier<Instant> clock;
   private final Path path;
+  private final Supplier<Instant> clock;
+  private final long scoreTtlInSeconds;
+  private final String delimiter;
+  private Instant firstContact;
+  private Instant lastContact;
+  private Duration offset;
 
-  private SocioPatternsDatasetFactory(Supplier<Instant> clock, Path path) {
+  private SocioPatternsDatasetFactory(
+      Path path, String delimiter, Supplier<Instant> clock, Duration scoreTtl) {
     this.contacts = new HashMap<>();
-    this.clock = clock;
     this.path = path;
+    this.delimiter = delimiter;
+    this.clock = clock;
+    this.scoreTtlInSeconds = scoreTtl.toSeconds();
+    this.firstContact = Instant.MAX;
+    this.lastContact = Instant.MIN;
   }
 
-  public static Dataset<Integer> newDataset(Supplier<Instant> clock, Path path) {
-    return new SocioPatternsDatasetFactory(clock, path).createDataset();
+  @Builder.Factory
+  protected static Dataset<Integer> socioPatternsDataset(
+      Path path, String delimiter, Supplier<Instant> clock, Duration scoreTtl) {
+    return new SocioPatternsDatasetFactory(path, delimiter, clock, scoreTtl).createDataset();
   }
 
   private static Instant merge(Instant oldValue, Instant newValue) {
@@ -38,11 +50,9 @@ public class SocioPatternsDatasetFactory extends DatasetFactory {
 
   @Override
   public void generateGraph(Graph<Integer, Edge<Integer>> target, Map<String, Integer> resultMap) {
-    Parsed parsed;
     try (BufferedReader reader = Files.newBufferedReader(path)) {
-      parsed = new Parsed(reader.readLine(), DEFAULT_DELIMITER);
-      addToGraph(target, parsed);
-      addContact(parsed);
+      reader.lines().forEach(line -> onLine(line, target));
+      calibrateTime();
     } catch (IOException exception) {
       throw new UncheckedIOException(exception);
     }
@@ -50,14 +60,19 @@ public class SocioPatternsDatasetFactory extends DatasetFactory {
 
   @Override
   protected RiskScore score(int node) {
-    // TODO Ttl
-    Duration lookBack = Duration.ofDays(Math.round(Math.random() * 13));
-    return RiskScore.builder().value(Math.random()).timestamp(clock.get().minus(lookBack)).build();
+    Duration lookBack = Duration.ofSeconds(Math.round(Math.random() * scoreTtlInSeconds));
+    return RiskScore.builder().value(Math.random()).timestamp(lastContact.minus(lookBack)).build();
   }
 
   @Override
   protected Instant timestamp(int node1, int node2) {
     return contacts.get(Set.of(node1, node2));
+  }
+
+  private void onLine(String line, Graph<Integer, Edge<Integer>> target) {
+    Parsed parsed = new Parsed(line, delimiter);
+    addToGraph(target, parsed);
+    addContact(parsed);
   }
 
   private void addToGraph(Graph<Integer, Edge<Integer>> target, Parsed parsed) {
@@ -67,8 +82,23 @@ public class SocioPatternsDatasetFactory extends DatasetFactory {
   }
 
   private void addContact(Parsed parsed) {
+    Instant timestamp = parsed.timestamp;
+    // Encourage max propagation: the earliest contact timestamp > the latest risk score timestamp.
+    firstContact = timestamp.isBefore(firstContact) ? timestamp : firstContact;
+    lastContact = timestamp.isAfter(lastContact) ? timestamp : lastContact;
     Set<Integer> nodes = Set.of(parsed.node1, parsed.node2);
-    contacts.merge(nodes, parsed.timestamp, SocioPatternsDatasetFactory::merge);
+    contacts.merge(nodes, timestamp, SocioPatternsDatasetFactory::merge);
+  }
+
+  private void calibrateTime() {
+    offset = Duration.between(lastContact, clock.get());
+    lastContact = lastContact.plus(offset);
+    firstContact = firstContact.plus(offset);
+    contacts.forEach((nodes, timestamp) -> contacts.computeIfPresent(nodes, this::addOffset));
+  }
+
+  private Instant addOffset(Set<Integer> nodes, Instant timestamp) {
+    return timestamp.plus(offset);
   }
 
   private static final class Parsed {
@@ -79,9 +109,9 @@ public class SocioPatternsDatasetFactory extends DatasetFactory {
 
     private Parsed(String string, String delimiter) {
       String[] args = string.split(delimiter);
-      node1 = Integer.parseInt(args[0]);
-      node2 = Integer.parseInt(args[1]);
-      timestamp = Instant.ofEpochSecond(Long.parseLong(args[2]));
+      timestamp = Instant.ofEpochSecond(Long.parseLong(args[0]));
+      node1 = Integer.parseInt(args[1]);
+      node2 = Integer.parseInt(args[2]);
     }
   }
 }
