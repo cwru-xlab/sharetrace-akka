c1af7564a3009406a9a75e685b8b069f410f2a70
rtatton <rdt17@protonmail.com>
2022-04-09 13:32:09 -0400

    Remove Experiment; refactor AbstractExperiment
    - Rename AbstractExperiment to Experiment and implement Runnable (for picoli CLI)
    - Extract all default values as constants
    
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/experiment/Experiment.java
+++ src/main/java/org/sharetrace/experiment/Experiment.java
@@ -1,6 +1,75 @@
 package org.sharetrace.experiment;
 
-public interface Experiment {
+import akka.actor.typed.Behavior;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.function.Supplier;
+import org.sharetrace.Runner;
+import org.sharetrace.data.Dataset;
+import org.sharetrace.model.message.AlgorithmMessage;
+import org.sharetrace.model.message.Parameters;
+import org.sharetrace.model.message.RiskScoreMessage;
+import org.sharetrace.util.IntervalCache;
 
-  void run();
+public abstract class Experiment<T> implements Runnable {
+
+  protected static final Duration DEFAULT_TTL = Duration.ofDays(14L);
+  protected static final double DEFAULT_SEND_TOLERANCE = 0.6d;
+  protected static final double DEFAULT_TRANSMISSION_RATE = 0.8d;
+  protected static final Duration DEFAULT_TIME_BUFFER = Duration.ofDays(2L);
+  protected static final long DEFAULT_NUM_INTERVALS = DEFAULT_TTL.toDays() + 1L;
+  protected static final long DEFAULT_BUFFER = 1L;
+  protected static final Duration DEFAULT_INTERVAL = Duration.ofDays(1L);
+  protected static final Duration DEFAULT_REFRESH_RATE = Duration.ofHours(1L);
+  protected static final long DEFAULT_SEED = 12345L;
+
+  @Override
+  public void run() {
+    Parameters parameters = parameters();
+    Dataset<T> dataset = newDataset(parameters);
+    Behavior<AlgorithmMessage> algorithm = newAlgorithm(dataset, parameters);
+    Runner.run(algorithm);
+  }
+
+  protected Supplier<Instant> clock() {
+    return Instant::now;
+  }
+
+  protected abstract Dataset<T> newDataset(Parameters parameters);
+
+  protected abstract Behavior<AlgorithmMessage> newAlgorithm(
+      Dataset<T> dataset, Parameters parameters);
+
+  protected Parameters parameters() {
+    return Parameters.builder()
+        .sendTolerance(DEFAULT_SEND_TOLERANCE)
+        .transmissionRate(DEFAULT_TRANSMISSION_RATE)
+        .timeBuffer(DEFAULT_TIME_BUFFER)
+        .scoreTtl(DEFAULT_TTL)
+        .contactTtl(DEFAULT_TTL)
+        .build();
+  }
+
+  protected Supplier<IntervalCache<RiskScoreMessage>> cacheFactory() {
+    return () ->
+        IntervalCache.<RiskScoreMessage>builder()
+            .nIntervals(DEFAULT_NUM_INTERVALS)
+            .nBuffer(DEFAULT_BUFFER)
+            .interval(DEFAULT_INTERVAL)
+            .refreshRate(DEFAULT_REFRESH_RATE)
+            .clock(clock())
+            .mergeStrategy(this::mergeStrategy)
+            .prioritizeReads(false)
+            .build();
+  }
+
+  protected RiskScoreMessage mergeStrategy(RiskScoreMessage oldScore, RiskScoreMessage newScore) {
+    double oldValue = oldScore.score().value();
+    double newValue = newScore.score().value();
+    Instant oldTimestamp = oldScore.score().timestamp();
+    Instant newTimestamp = newScore.score().timestamp();
+    boolean isHigher = oldValue < newValue;
+    boolean isOlder = oldValue == newValue && oldTimestamp.isAfter(newTimestamp);
+    return isHigher || isOlder ? newScore : oldScore;
+  }
 }
