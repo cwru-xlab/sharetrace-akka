7a80724162b088af5c91d6c43f965890a3fec01f
rtatton <rdt17@protonmail.com>
2022-05-01 12:33:23 -0400

    Forgot to push (see previous commit)
    Signed-off-by: rtatton <rdt17@protonmail.com>

---
+++ src/main/java/org/sharetrace/model/graph/Loggable.java
@@ -0,0 +1,12 @@
+package org.sharetrace.model.graph;
+
+public enum Loggable {
+  SEND_CACHED,
+  SEND_CURRENT,
+  PROPAGATE,
+  RECEIVE,
+  UPDATE,
+  CURRENT_REFRESH,
+  CONTACTS_REFRESH,
+  NEW_CONTACT,
+}
