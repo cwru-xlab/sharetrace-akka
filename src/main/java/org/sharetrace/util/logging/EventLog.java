fa072ad2247635b9947ecee87d3d17216f958472
rtatton <rdt17@protonmail.com>
2022-05-02 13:39:39 -0400

    Refactor EventLog constructors
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/util/logging/EventLog.java
+++ src/main/java/org/sharetrace/util/logging/EventLog.java
@@ -1,51 +1,41 @@
 package org.sharetrace.util.logging;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.EnumSet;
 import java.util.Objects;
-import java.util.Set;
 
 public final class EventLog<E extends Enum<E> & LoggableEvent> {
 
-  private final Set<E> enabled;
+  private final Collection<E> enabled;
 
-  private EventLog(Set<E> enabled) {
-    this.enabled = Collections.unmodifiableSet(enabled);
+  private EventLog(Collection<E> enabled) {
+    this.enabled = enabled;
   }
 
-  public static <E extends Enum<E> & LoggableEvent> EventLog<E> enable(EnumSet<E> enable) {
-    return new EventLog<>(enable);
+  public static <E extends Enum<E> & LoggableEvent> EventLog<E> enable(Collection<E> enable) {
+    return new EventLog<>(EnumSet.copyOf(enable));
   }
 
   @SafeVarargs
   public static <E extends Enum<E> & LoggableEvent> EventLog<E> enable(E first, E... rest) {
-    return enable(EnumSet.of(first, rest));
-  }
-
-  public static <E extends Enum<E> & LoggableEvent> EventLog<E> enable(Collection<E> enable) {
-    return enable(EnumSet.copyOf(enable));
+    return new EventLog<>(EnumSet.of(first, rest));
   }
 
   public static <E extends Enum<E> & LoggableEvent> EventLog<E> enableAll(Class<E> clazz) {
-    return enable(EnumSet.allOf(clazz));
+    return new EventLog<>(EnumSet.allOf(clazz));
   }
 
-  public static <E extends Enum<E> & LoggableEvent> EventLog<E> disable(EnumSet<E> disable) {
-    return enable(EnumSet.complementOf(disable));
+  public static <E extends Enum<E> & LoggableEvent> EventLog<E> disable(Collection<E> disable) {
+    return new EventLog<>(EnumSet.complementOf(EnumSet.copyOf(disable)));
   }
 
   @SafeVarargs
   public static <E extends Enum<E> & LoggableEvent> EventLog<E> disable(E first, E... rest) {
-    return disable(EnumSet.of(first, rest));
-  }
-
-  public static <E extends Enum<E> & LoggableEvent> EventLog<E> disable(Collection<E> disable) {
-    return disable(EnumSet.copyOf(disable));
+    return new EventLog<>(EnumSet.complementOf(EnumSet.of(first, rest)));
   }
 
   public static <E extends Enum<E> & LoggableEvent> EventLog<E> disableAll(Class<E> clazz) {
-    return enable(EnumSet.noneOf(clazz));
+    return new EventLog<>(EnumSet.noneOf(clazz));
   }
 
   public boolean contains(E event) {
