0b9c1279db780347c934a7cbaeb474c4d979a001
rtatton <rdt17@protonmail.com>
2022-05-08 15:55:12 -0400

    Add convenience constructor and loggables
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/experiment/RuntimeExperiment.java
+++ src/main/java/org/sharetrace/experiment/RuntimeExperiment.java
@@ -1,6 +1,9 @@
 package org.sharetrace.experiment;
 
+import java.util.Set;
 import java.util.stream.IntStream;
+import org.sharetrace.logging.Loggable;
+import org.sharetrace.logging.metrics.RuntimeMetric;
 
 public class RuntimeExperiment extends SyntheticExperiment {
 
@@ -8,6 +11,10 @@ public class RuntimeExperiment extends SyntheticExperiment {
   private final int maxNodes;
   private final int stepNodes;
   private final int nRepeats;
+
+  public RuntimeExperiment(GraphType graphType, int nNodes, long seed) {
+    this(graphType, nNodes, nNodes, 1, 1, seed);
+  }
 
   public RuntimeExperiment(
       GraphType graphType, int minNodes, int maxNodes, int stepNodes, int nRepeats, long seed) {
@@ -20,10 +27,16 @@ public class RuntimeExperiment extends SyntheticExperiment {
 
   @Override
   public void run() {
-    for (int n = minNodes; n < maxNodes; n += stepNodes) {
+    for (int n = minNodes; n <= maxNodes; n += stepNodes) {
       nNodes = n;
       nEdges = n * 2;
       IntStream.range(0, nRepeats).forEach(x -> super.run());
     }
+  }
+
+  @Override
+  protected Set<Class<? extends Loggable>> loggable() {
+    // Logging events and graph metrics becomes too expensive for large graphs
+    return Set.of(RuntimeMetric.class);
   }
 }
