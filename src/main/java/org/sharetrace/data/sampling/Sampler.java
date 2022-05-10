9757ea50d5a1740166ac0db53a869bcc26e4d01e
rtatton <rdt17@protonmail.com>
2022-05-10 16:18:54 -0400

    Add Stream support
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/data/sampling/Sampler.java
+++ src/main/java/org/sharetrace/data/sampling/Sampler.java
@@ -1,7 +1,13 @@
 package org.sharetrace.data.sampling;
 
+import java.util.stream.Stream;
+
 @FunctionalInterface
 public interface Sampler<T> {
+
+  default Stream<T> stream() {
+    return Stream.generate(this::sample);
+  }
 
   T sample();
 }
