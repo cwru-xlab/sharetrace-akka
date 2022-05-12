f6c9c5bb744cd6cc687cc78879c7ca21b75ffc7d
rtatton <rdt17@protonmail.com>
2022-05-12 09:16:08 -0400

    Add SocioPatterns graph types
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/experiment/GraphType.java
+++ src/main/java/org/sharetrace/experiment/GraphType.java
@@ -1,9 +1,18 @@
 package org.sharetrace.experiment;
 
 public enum GraphType {
+  // Synthetic
   BARABASI_ALBERT,
   GNM_RANDOM,
   RANDOM_REGULAR,
   SCALE_FREE,
-  WATTS_STROGATZ
+  WATTS_STROGATZ,
+  // SocioPatterns
+  INVS13,
+  INVS15,
+  LH10,
+  LYON_SCHOOL,
+  SFHH,
+  THIERS11,
+  THIERS12
 }
