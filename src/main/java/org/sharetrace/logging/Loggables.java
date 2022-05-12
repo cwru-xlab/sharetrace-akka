edc9955b2c4707a82290550e7584af7a974e64e5
rtatton <rdt17@protonmail.com>
2022-05-12 15:11:56 -0400

    Add convenience methods
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/logging/Loggables.java
+++ src/main/java/org/sharetrace/logging/Loggables.java
@@ -13,7 +13,7 @@ public final class Loggables {
   private final Supplier<Logger> logger;
 
   private Loggables(Set<Class<? extends Loggable>> loggable, Supplier<Logger> logger) {
-    this.loggable = loggable;
+    this.loggable = Objects.requireNonNull(loggable);
     this.logger = Objects.requireNonNull(logger);
   }
 
@@ -35,6 +35,14 @@ public final class Loggables {
     }
   }
 
+  public void info(String messageAndKey, Loggable value) {
+    info(messageAndKey, messageAndKey, TypedSupplier.of(value));
+  }
+
+  public void info(String messageAndKey, TypedSupplier<? extends Loggable> supplier) {
+    info(messageAndKey, messageAndKey, supplier);
+  }
+
   public void debug(String message, String key, Loggable value) {
     debug(message, key, TypedSupplier.of(value));
   }
@@ -43,6 +51,14 @@ public final class Loggables {
     if (loggable.contains(supplier.getType())) {
       logger.get().debug(message, StructuredArguments.value(key, supplier.get()));
     }
+  }
+
+  public void debug(String messageAndKey, Loggable value) {
+    debug(messageAndKey, messageAndKey, TypedSupplier.of(value));
+  }
+
+  public void debug(String messageAndKey, TypedSupplier<? extends Loggable> supplier) {
+    debug(messageAndKey, messageAndKey, supplier);
   }
 
   public Set<Class<? extends Loggable>> loggable() {
