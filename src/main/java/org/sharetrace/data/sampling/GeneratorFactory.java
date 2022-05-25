074ba0359c4aa3d081cf9013cff6e1be54f08b70
rtatton <rdt17@protonmail.com>
2022-05-25 15:09:29 -0400

    Remove unused method
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/data/sampling/GeneratorFactory.java
+++ src/main/java/org/sharetrace/data/sampling/GeneratorFactory.java
@@ -5,9 +5,5 @@ import org.apache.commons.math3.random.RandomGenerator;
 @FunctionalInterface
 public interface GeneratorFactory {
 
-  default RandomGenerator create(int seed) {
-    return create((long) seed);
-  }
-
   RandomGenerator create(long seed);
 }
