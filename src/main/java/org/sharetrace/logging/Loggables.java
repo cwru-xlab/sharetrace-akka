a13c1d11920e7dacf013fac368f2546131d27087
rtatton <rdt17@protonmail.com>
2022-07-12 23:33:20 -0400

    Modify constructor
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/logging/Loggables.java
+++ src/main/java/org/sharetrace/logging/Loggables.java
@@ -1,6 +1,5 @@
 package org.sharetrace.logging;
 
-import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Supplier;
@@ -14,7 +13,7 @@ public final class Loggables {
   private final Supplier<Logger> logger;
 
   private Loggables(Set<Class<? extends Loggable>> loggable, Supplier<Logger> logger) {
-    this.loggable = new HashSet<>(loggable);
+    this.loggable = Set.copyOf(loggable);
     this.logger = Objects.requireNonNull(logger);
   }
 
