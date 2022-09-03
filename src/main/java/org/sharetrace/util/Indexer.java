d6ae021554c579255708f122f3215f39c6654851
rtatton <rdt17@protonmail.com>
2022-09-03 18:25:52 -0400

    Simplify Indexer
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/util/Indexer.java
+++ src/main/java/org/sharetrace/util/Indexer.java
@@ -1,26 +1,20 @@
 package org.sharetrace.util;
 
-import com.google.common.base.MoreObjects;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import java.util.Map;
 
 public class Indexer<T> {
 
-  protected static final int DEFAULT_INITIAL_CAPACITY = 16;
   private final Map<T, Integer> index;
   private int value;
 
   public Indexer() {
-    this(DEFAULT_INITIAL_CAPACITY);
+    this(Object2IntOpenHashMap.DEFAULT_INITIAL_SIZE);
   }
 
   public Indexer(int capacity) {
-    index = newIndex(capacity);
+    index = new Object2IntOpenHashMap<>(capacity);
     value = 0;
-  }
-
-  protected Map<T, Integer> newIndex(int capacity) {
-    return new Object2IntOpenHashMap<>(capacity);
   }
 
   public int index(T value) {
