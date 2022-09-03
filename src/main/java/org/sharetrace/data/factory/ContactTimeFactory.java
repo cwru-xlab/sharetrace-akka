6796700463e453e00a6389b95d857ca7a081d503
rtatton <rdt17@protonmail.com>
2022-09-03 11:18:40 -0400

    Add fromSupplier() converter
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/data/factory/ContactTimeFactory.java
+++ src/main/java/org/sharetrace/data/factory/ContactTimeFactory.java
@@ -1,9 +1,14 @@
 package org.sharetrace.data.factory;
 
 import java.time.Instant;
+import java.util.function.Supplier;
 
 @FunctionalInterface
 public interface ContactTimeFactory {
+
+  static ContactTimeFactory fromSupplier(Supplier<Instant> supplier) {
+    return (x, xx) -> supplier.get();
+  }
 
   Instant getContactTime(int user1, int user2);
 }
--- src/main/java/org/sharetrace/data/factory/RiskScoreFactory.java
+++ src/main/java/org/sharetrace/data/factory/RiskScoreFactory.java
@@ -1,9 +1,14 @@
 package org.sharetrace.data.factory;
 
+import java.util.function.Supplier;
 import org.sharetrace.message.RiskScore;
 
 @FunctionalInterface
 public interface RiskScoreFactory {
+
+  static RiskScoreFactory fromSupplier(Supplier<RiskScore> supplier) {
+    return x -> supplier.get();
+  }
 
   RiskScore getRiskScore(int user);
 }
