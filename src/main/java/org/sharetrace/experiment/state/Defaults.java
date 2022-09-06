ed8523b8a3671e26621d3f5617aef28ebaa52379
rtatton <rdt17@protonmail.com>
2022-09-06 14:41:38 -0400

    Merge DefaultStateFactories into Defaults
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/experiment/state/Defaults.java
+++ src/main/java/org/sharetrace/experiment/state/Defaults.java
@@ -1,12 +1,22 @@
 package org.sharetrace.experiment.state;
 
 import com.google.common.math.DoubleMath;
+import java.nio.file.Path;
 import java.time.Duration;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import org.immutables.builder.Builder;
+import org.jgrapht.graph.DefaultEdge;
 import org.sharetrace.data.Dataset;
+import org.sharetrace.data.FileDataset;
+import org.sharetrace.data.SampledDataset;
+import org.sharetrace.data.factory.GraphGeneratorBuilder;
+import org.sharetrace.data.factory.GraphGeneratorFactory;
 import org.sharetrace.experiment.ExperimentContext;
+import org.sharetrace.experiment.GraphType;
 import org.sharetrace.logging.metrics.RuntimeMetric;
 import org.sharetrace.logging.metrics.SizeMetrics;
 import org.sharetrace.logging.metrics.TopologyMetric;
@@ -16,19 +26,113 @@ import org.sharetrace.message.RiskScoreMessage;
 import org.sharetrace.message.UserParameters;
 import org.sharetrace.util.CacheParameters;
 
+@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public final class Defaults {
 
+  private static final String WHITESPACE_DELIMITER = "\\s+";
   private static final double MIN_BASE = Math.log(1.1);
   private static final double MAX_BASE = Math.log(10d);
   private static final double DECAY_RATE = 1.75E-7;
   private static final Duration DEFAULT_TTL = Duration.ofDays(14L);
-  private static final CacheParameters<RiskScoreMessage> CACHE_PARAMETERS = newCacheParameters();
   private static final MessageParameters MESSAGE_PARAMETERS = newMessageParameters();
+  private static final CacheParameters<RiskScoreMessage> CACHE_PARAMETERS = newCacheParameters();
   private static final ExperimentContext CONTEXT = ExperimentContext.create();
   private static final ExperimentContext RUNTIME_CONTEXT = newRuntimeContext();
   private static final ExperimentContext PARAMETERS_CONTEXT = newParametersContext();
 
   private Defaults() {}
+
+  @Builder.Factory()
+  public static ExperimentState defaultFileState(
+      GraphType graphType, Optional<String> delimiter, Path path) {
+    return ExperimentState.builder(ExperimentContext.create())
+        .graphType(graphType)
+        .dataset(context -> fileDataset(context, delimiter.orElse(WHITESPACE_DELIMITER), path))
+        .build();
+  }
+
+  @Builder.Factory
+  public static Dataset fileDataset(DatasetContext context, String delimiter, Path path) {
+    return FileDataset.builder()
+        .delimiter(delimiter)
+        .path(path)
+        .addAllLoggable(context.loggable())
+        .referenceTime(context.referenceTime())
+        .riskScoreFactory(context.riskScoreFactory())
+        .build();
+  }
+
+  @Builder.Factory
+  public static ExperimentState defaultSampledState(
+      GraphType graphType, int numNodes, Optional<GraphGeneratorFactory> graphGeneratorFactory) {
+    return ExperimentState.builder(ExperimentContext.create())
+        .graphType(graphType)
+        .dataset(context -> sampledDataset(context, numNodes, graphGeneratorFactory))
+        .build();
+  }
+
+  @Builder.Factory
+  public static Dataset sampledDataset(
+      DatasetContext context, int numNodes, Optional<GraphGeneratorFactory> generatorFactory) {
+    return SampledDataset.builder()
+        .addAllLoggable(context.loggable())
+        .riskScoreFactory(context.riskScoreFactory())
+        .contactTimeFactory(context.contactTimeFactory())
+        .graphGeneratorFactory(generatorFactory.orElseGet(defaultFactory(context)))
+        .numNodes(numNodes)
+        .build();
+  }
+
+  private static Supplier<GraphGeneratorFactory> defaultFactory(DatasetContext cxt) {
+    return () ->
+        nNodes ->
+            GraphGeneratorBuilder.<Integer, DefaultEdge>create(cxt.graphType(), nNodes, cxt.seed())
+                .numEdges(nNodes * 2)
+                .degree(4)
+                .numNearestNeighbors(2)
+                .numInitialNodes(2)
+                .numNewEdges(2)
+                .rewiringProbability(0.3)
+                .build();
+  }
+
+  @Builder.Factory
+  public static ExperimentState defaultParametersState(
+      GraphType graphType,
+      int numNodes,
+      Optional<GraphGeneratorFactory> graphGeneratorFactory,
+      float transmissionRate,
+      float sendCoefficient) {
+    return ExperimentState.builder(parametersContext())
+        .graphType(graphType)
+        .messageParameters(
+            messageParameters()
+                .withTransmissionRate(transmissionRate)
+                .withSendCoefficient(sendCoefficient))
+        .dataset(context -> sampledDataset(context, numNodes, graphGeneratorFactory))
+        .build();
+  }
+
+  public static ExperimentContext parametersContext() {
+    return PARAMETERS_CONTEXT;
+  }
+
+  public static MessageParameters messageParameters() {
+    return MESSAGE_PARAMETERS;
+  }
+
+  @Builder.Factory
+  public static ExperimentState defaultRuntimeState(
+      GraphType graphType, int numNodes, Optional<GraphGeneratorFactory> graphGeneratorFactory) {
+    return ExperimentState.builder(runtimeContext())
+        .graphType(graphType)
+        .dataset(context -> sampledDataset(context, numNodes, graphGeneratorFactory))
+        .build();
+  }
+
+  public static ExperimentContext runtimeContext() {
+    return RUNTIME_CONTEXT;
+  }
 
   public static CacheParameters<RiskScoreMessage> cacheParameters() {
     return CACHE_PARAMETERS;
@@ -46,14 +150,6 @@ public final class Defaults {
     double targetBase = Math.max(MIN_BASE, MAX_BASE - DECAY_RATE * nContacts);
     long timeout = (long) Math.ceil(Math.log(nContacts) / targetBase);
     return Duration.ofSeconds(timeout);
-  }
-
-  public static ExperimentContext runtimeContext() {
-    return RUNTIME_CONTEXT;
-  }
-
-  public static ExperimentContext parametersContext() {
-    return PARAMETERS_CONTEXT;
   }
 
   private static ExperimentContext newParametersContext() {
@@ -97,10 +193,6 @@ public final class Defaults {
   private static boolean isApproxEqual(RiskScoreMessage msg1, RiskScoreMessage msg2) {
     return DoubleMath.fuzzyEquals(
         msg1.score().value(), msg2.score().value(), MESSAGE_PARAMETERS.scoreTolerance());
-  }
-
-  public static MessageParameters messageParameters() {
-    return MESSAGE_PARAMETERS;
   }
 
   private static MessageParameters newMessageParameters() {
