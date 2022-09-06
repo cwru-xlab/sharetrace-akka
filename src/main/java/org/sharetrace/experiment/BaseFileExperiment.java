7f94df692f3ca74d64abb1bdda071cd09c5cceac
rtatton <rdt17@protonmail.com>
2022-09-06 16:38:42 -0400

    Fix FileExperiment
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/experiment/BaseFileExperiment.java
+++ src/main/java/org/sharetrace/experiment/BaseFileExperiment.java
@@ -1,15 +1,16 @@
 package org.sharetrace.experiment;
 
-import java.util.stream.IntStream;
 import org.immutables.value.Value;
 import org.sharetrace.experiment.state.ExperimentState;
+import org.sharetrace.util.Range;
 
 @Value.Immutable
 abstract class BaseFileExperiment implements Experiment {
 
   @Override
   public void run(ExperimentState initialState) {
-    IntStream.of(numIterations()).forEach(x -> initialState.run());
+    // Only need to generate a new state ID.
+    Range.of(numIterations()).forEach(x -> initialState.toBuilder().build().run());
   }
 
   @Value.Parameter
