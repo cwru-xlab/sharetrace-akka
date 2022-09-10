6d2faee7b7e7bae7130f1cc0b6871eb41662372a
rtatton <rdt17@protonmail.com>
2022-09-09 23:34:42 -0400

    Add null checks
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/graph/ContactNetworkHelper.java
+++ src/main/java/org/sharetrace/graph/ContactNetworkHelper.java
@@ -25,6 +25,7 @@ import org.sharetrace.logging.metrics.LoggableMetric;
 import org.sharetrace.logging.metrics.ScoringMetrics;
 import org.sharetrace.logging.metrics.SizeMetrics;
 import org.sharetrace.logging.metrics.TopologyMetric;
+import org.sharetrace.util.Checks;
 import org.sharetrace.util.TypedSupplier;
 
 public final class ContactNetworkHelper {
@@ -40,15 +41,18 @@ public final class ContactNetworkHelper {
   public static ContactNetworkHelper of(
       GraphGenerator<Integer, DefaultEdge, ?> graphGenerator,
       Set<Class<? extends Loggable>> loggable) {
+    Logger logger = Logging.metricsLogger(loggable); // Create early to check for null.
+    Checks.isNotNull(graphGenerator, "graphGenerator");
     Graph<Integer, DefaultEdge> contactNetwork = GraphFactory.newUndirectedGraph();
     graphGenerator.generateGraph(contactNetwork);
-    return new ContactNetworkHelper(contactNetwork, Logging.metricsLogger(loggable));
+    return new ContactNetworkHelper(contactNetwork, logger);
   }
 
-  public Set<Contact> contacts(ContactTimeFactory timeFactory) {
+  public Set<Contact> contacts(ContactTimeFactory contactTimeFactory) {
+    Checks.isNotNull(contactTimeFactory, "contactTimeFactory");
     Set<DefaultEdge> edges = contactNetwork.edgeSet();
     return edges.stream()
-        .map(edge -> toContact(edge, timeFactory))
+        .map(edge -> toContact(edge, contactTimeFactory))
         .collect(ObjectOpenHashSet.toSetWithExpectedSize(edges.size()));
   }
 
