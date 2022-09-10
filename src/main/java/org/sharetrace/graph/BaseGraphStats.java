1b1bffd3fd9e18b8dcbc38dc88436ccbb15dd856
rtatton <rdt17@protonmail.com>
2022-09-09 23:35:22 -0400

    Simplify HarmonicCentrality
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/graph/BaseGraphStats.java
+++ src/main/java/org/sharetrace/graph/BaseGraphStats.java
@@ -1,14 +1,12 @@
 package org.sharetrace.graph;
 
 import it.unimi.dsi.fastutil.floats.FloatArrayList;
-import it.unimi.dsi.fastutil.objects.Object2DoubleOpenHashMap;
 import java.util.Collection;
 import java.util.List;
 import org.immutables.value.Value;
 import org.jgrapht.Graph;
 import org.jgrapht.GraphMetrics;
 import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
-import org.jgrapht.alg.interfaces.ShortestPathAlgorithm.SingleSourcePaths;
 import org.jgrapht.alg.interfaces.VertexScoringAlgorithm;
 import org.jgrapht.alg.scoring.ClosenessCentrality;
 import org.jgrapht.alg.scoring.ClusteringCoefficient;
@@ -144,7 +142,6 @@ abstract class BaseGraphStats<V, E> {
     return new GraphMeasurer<>(graph(), shortestPath());
   }
 
-  // Copied from JGraphT's implementation, but reuses a ShortestPathAlgorithm instance.
   private static final class HarmonicCentrality<V, E> extends ClosenessCentrality<V, E> {
 
     private final ShortestPathAlgorithm<V, E> shortestPaths;
@@ -155,26 +152,8 @@ abstract class BaseGraphStats<V, E> {
     }
 
     @Override
-    protected void compute() {
-      // Modified from original to use fastutil Map.
-      scores = new Object2DoubleOpenHashMap<>();
-      int n = graph.vertexSet().size();
-      SingleSourcePaths<V, E> paths;
-      double sum;
-      for (V v : graph.vertexSet()) {
-        sum = 0d;
-        paths = shortestPaths.getPaths(v);
-        for (V u : graph.vertexSet()) {
-          if (!u.equals(v)) {
-            sum += 1.0 / paths.getWeight(u);
-          }
-        }
-        if (normalize && n > 1) {
-          scores.put(v, sum / (n - 1));
-        } else {
-          scores.put(v, sum);
-        }
-      }
+    protected ShortestPathAlgorithm<V, E> getShortestPathAlgorithm() {
+      return shortestPaths;
     }
   }
 }
