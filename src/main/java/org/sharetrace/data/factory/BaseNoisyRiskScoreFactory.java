81daee1ebbebc35f602d3f43e400f414a9d3b012
rtatton <rdt17@protonmail.com>
2022-09-16 16:39:03 -0400

    Ensure noisy values are in range
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/main/java/org/sharetrace/data/factory/BaseNoisyRiskScoreFactory.java
+++ src/main/java/org/sharetrace/data/factory/BaseNoisyRiskScoreFactory.java
@@ -8,10 +8,14 @@ import org.sharetrace.model.RiskScore;
 @Value.Immutable(copy = true)
 abstract class BaseNoisyRiskScoreFactory implements RiskScoreFactory {
 
+  private static float constrain(double value) {
+    return (float) Math.max(RiskScore.MIN_VALUE, Math.min(RiskScore.MAX_VALUE, value));
+  }
+
   @Override
   public RiskScore riskScore(int user) {
     RiskScore score = scoreFactory().riskScore(user);
-    return score.withValue((float) (score.value() + noise().sample()));
+    return score.withValue(constrain(score.value() + noise().sample()));
   }
 
   @Value.Parameter
