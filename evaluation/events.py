fa43aa06632ea5590534764e971db196dc35e135
rtatton <rdt17@protonmail.com>
2022-09-20 00:00:05 -0400

    Add clarifying docstring
    This is mainly for me so that I remember what stuff does.
    
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- evaluation/events.py
+++ evaluation/events.py
@@ -23,6 +23,7 @@ ONE = np.int8(1)
 
 
 class Event(str, enum.Enum):
+    """Typed of events logged by user actors."""
     CONTACT = "ContactEvent"
     CONTACTS_REFRESH = "ContactsRefreshEvent"
     CURRENT_REFRESH = "CurrentRefreshEvent"
@@ -46,17 +47,18 @@ class Event(str, enum.Enum):
 _EVENTS = {v: i for i, v in enumerate(Event)}
 
 
-def event_stream(logdir: os.PathLike | str) -> Iterable[EventRecord]:
+def stream(logdir: os.PathLike | str) -> Iterable[EventRecord]:
+    """Returns a stream of event records from the logging directory."""
     logdir = pathlib.Path(logdir).absolute()
     zipped, unzipped = _split(logdir.iterdir(), _is_zipped)
     # Iterate over zipped first! Unzipped events occur last.
     with tempfile.TemporaryDirectory() as tmpdir:
         tmpdir = pathlib.Path(tmpdir).absolute()
         _unzip(zipped, tmpdir)
-        for event in _stream(tmpdir.iterdir()):
-            yield event
-    for event in _stream(unzipped):
-        yield event
+        for record in _stream(tmpdir.iterdir()):
+            yield record
+    for record in _stream(unzipped):
+        yield record
 
 
 def _stream(filenames: Iterable[pathlib.Path]) -> Iterable[EventRecord]:
@@ -84,69 +86,83 @@ def _split(it: Iterable, predicate: Predicate) -> tuple[Iterable, Iterable]:
 
 
 class EventCounter(collections.Counter):
+    """Counts the number of occurrences of each event type."""
 
     def __setitem__(self, key, value):
         if key in _EVENTS:
             super().__setitem__(key, value)
         else:
-            raise ValueError("'key' must be an Event instance")
+            raise ValueError(
+                f"'key' must be an {Event.__name__} instance")
 
     @property
     def num_sent_to_contacts(self) -> int:
+        """Returns the number of messages sent to a contact."""
         return self.num_curr_sent + self.num_cached_sent
 
     @property
     def num_received(self) -> int:
+        """Returns the number of messages received."""
         return self[Event.RECEIVE]
 
     @property
     def num_curr_sent(self) -> int:
+        """Returns the number of current messages."""
         return self[Event.SEND_CURRENT]
 
     @property
     def num_cached_sent(self) -> int:
+        """Returns the number of cached messages."""
         return self[Event.SEND_CACHED]
 
     @property
     def num_propagated(self) -> int:
+        """Returns the number of propagated messages."""
         return self[Event.PROPAGATE]
 
     @property
     def num_updates(self) -> int:
+        """Returns the number of user actor updates."""
         return self[Event.UPDATE]
 
     @property
     def num_contacts(self) -> int:
+        """Returns the number of contacts."""
         # Each user logs a contact, so each contact is double counted.
         return int(self[Event.CONTACT] / 2)
 
     @property
     def num_contact_refreshes(self) -> int:
+        """Returns the number of contact refreshes."""
         return self[Event.CONTACTS_REFRESH]
 
     @property
     def num_curr_refreshes(self) -> int:
+        """Returns the number of current message refreshes."""
         return self[Event.CURRENT_REFRESH]
 
     @property
-    def num_not_to_contacts(self) -> int:
+    def num_not_sent_to_contacts(self) -> int:
+        """Returns the number of messages not sent to a contact."""
         # Each user sends a message, so double the number of contacts.
         return 2 * self.num_contacts - self.num_sent_to_contacts
 
 
-# Callbacks
-
 class EventCallback(Callable):
+    """A callable that processes event records."""
 
     @abc.abstractmethod
     def __call__(self, record: EventRecord, **kwargs) -> None:
+        """Processes the record."""
         pass
 
     def on_complete(self) -> None:
+        """Finalizes the callback for inspection after processing records."""
         pass
 
 
 class EventCounterCallback(EventCounter, EventCallback):
+    """An event callback for counting the occurrences of events."""
 
     def __call__(self, record: EventRecord, **kwargs) -> None:
         self.update((Event.of(record),))
@@ -164,6 +180,13 @@ class UserUpdates:
 
 
 class UserUpdatesCallback(EventCallback, Sized):
+    """An event callback for tracking user updates.
+
+    Attributes:
+       updates: An array where the i-th entry is number of updates of user i.
+       initials: An array where the i-th entry is the initial value of user i.
+       finals: An array where the i-th entry is the final value of user i.
+    """
     __slots__ = "updates", "initials", "finals", "_n"
 
     def __init__(self) -> None:
@@ -201,17 +224,21 @@ class UserUpdatesCallback(EventCallback, Sized):
 
 
 class TimelineCallback(EventCallback):
-    __slots__ = "_events", "_repeats", "idx"
+    """An event callback for recording the order of events."""
+    __slots__ = "_events", "_repeats", "_idx"
 
     def __init__(self):
         self._events: list[Event | None] | np.ndarray = [None]
         self._repeats: list[int] | np.ndarray = [0]
-        self.idx: dict | np.ndarray = dict()
+        self._idx: dict | np.ndarray = dict()
 
     def __call__(self, record: EventRecord, **kwargs) -> None:
-        if (name := Event.of(record)) not in self.idx:
-            self.idx[name] = len(self.idx)
-        if (label := self.idx[name]) == self._events[-1]:
+        if (name := Event.of(record)) not in self._idx:
+            # Encode the event as an integer.
+            self._idx[name] = len(self._idx)
+        # If the previous event was also this type...
+        if (label := self._idx[name]) == self._events[-1]:
+            # ...increment its count.
             self._repeats[-1] += 1
         else:
             self._events.append(label)
@@ -220,19 +247,45 @@ class TimelineCallback(EventCallback):
     def on_complete(self) -> None:
         self._events = np.array(self._events[1:])
         self._repeats = np.array(self._repeats[1:])
-        self.idx = np.array(list(self.idx))
+        self._idx = np.array(list(self._idx))
 
     def flatten(self, labeled: bool = False) -> np.ndarray:
+        """Returns a 1D array of events.
+
+        Args:
+            labeled: If true, entries are the names of the events. Otherwise,
+                entries are encoded integers.
+        """
         return np.repeat(self._get_events(labeled), self._repeats)
 
     def run_length_encoded(self, labeled: bool = False) -> np.ndarray:
+        """Returns a 2D array of run-length encoded events.
+
+        An entry (`e`, `n`) corresponds to the event `e` and the number `n` of
+        times it occurred consecutively.
+
+        Args:
+            labeled: If true, entries are the names of the events. Otherwise,
+                entries are encoded integers.
+        """
         return np.array(list(zip(self._get_events(labeled), self._repeats)))
 
     def _get_events(self, labeled: bool = False) -> np.ndarray:
-        return self.idx[self._events] if labeled else self._events
+        return self._idx[self._events] if labeled else self._events
 
 
 class ReachabilityCallback(EventCallback):
+    """An event callback for computing reachability metrics.
+
+    Attributes:
+        adj: The message-reachability adjacency matrix. Entry `ij` is 1 if
+            the initial message of user `i` reached user `j`,
+            and 0 otherwise.
+        msg_idx: A dictionary that maps a message ID to the origin user.
+        msgs: A list of (source, destination) pairs that passed the initial
+            score of user i.
+    """
+
     __slots__ = (
         "adj",
         "msg_idx",
@@ -242,11 +295,10 @@ class ReachabilityCallback(EventCallback):
         "_influence",
         "_source_size")
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self) -> None:
         self.adj: sparse.csr_matrix | None = None
         self.msg_idx: dict[str, np.uint32] = {}
-        self.msgs: dict | list = collections.defaultdict(list)
+        self.msgs: dict[str, list] | list = collections.defaultdict(list)
         self._msg_reach: np.ndarray | None = None
         self._reach_ratio: float | None = None
         self._influence: np.ndarray | None = None
@@ -273,7 +325,6 @@ class ReachabilityCallback(EventCallback):
         self._compute_msg_reaches()
 
     def _to_numpy(self) -> None:
-        # msgs[i] = (to, from) pairs that sent message starting from user i.
         self.msgs = [np.array(self.msgs[v]) for v in range(len(self.msgs))]
 
     def influence(self, user: int | None = None) -> int | np.ndarray:
@@ -331,6 +382,7 @@ class ReachabilityCallback(EventCallback):
 
 
 def edges_to_adj(edges: set[tuple]) -> tuple[dict, sparse.spmatrix]:
+    """Encodes a set of edges as an adjacency matrix."""
     idx = index_edges(edges)
     adj = np.zeros((len(idx), len(idx)), dtype=np.int8)
     for v1, v2 in edges:
@@ -339,11 +391,13 @@ def edges_to_adj(edges: set[tuple]) -> tuple[dict, sparse.spmatrix]:
 
 
 def index_edges(edges: set[tuple]) -> dict[int, int]:
+    """Encodes the vertices of the edges as 0-based integers."""
     return {v: i for i, v in enumerate(set(itertools.chain(*edges)))}
 
 
-def analyze_events(logdir: str, *callbacks: EventCallback) -> None:
-    for event, callback in itertools.product(event_stream(logdir), callbacks):
+def analyze(logdir: str, *callbacks: EventCallback) -> None:
+    """Analyze the event logs in the logging directory."""
+    for event, callback in itertools.product(stream(logdir), callbacks):
         callback(event)
     for callback in callbacks:
         callback.on_complete()
