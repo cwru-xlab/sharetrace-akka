28358245b1536a63c4bc99f3288632f99b5221f7
rtatton <rdt17@proton.me>
2023-05-21 21:49:03 -0400

    refactor: avoid unnecessary array creation with varargs

--- src/main/java/io/sharetrace/util/Checks.java
+++ src/main/java/io/sharetrace/util/Checks.java
@@ -7,19 +7,22 @@ public final class Checks {
   private Checks() {}
 
   public static <T extends Comparable<T>> T checkRange(T value, Range<T> range, String name) {
-    checkArgs(range.contains(value), "%s must be in the range %s; got %s", name, range, value);
+    if (!range.contains(value)) {
+      throw new IllegalArgumentException(
+          String.format("%s must be in the range %s; got %s", name, range, value));
+    }
     return value;
   }
 
-  public static void checkState(boolean condition, String template, Object... args) {
+  public static void checkState(boolean condition, String template, Object arg) {
     if (!condition) {
-      throw new IllegalStateException(String.format(template, args));
+      throw new IllegalStateException(String.format(template, arg));
     }
   }
 
-  public static void checkArgs(boolean condition, String template, Object... args) {
+  public static void checkState(boolean condition, String template, Object arg1, Object arg2) {
     if (!condition) {
-      throw new IllegalArgumentException(String.format(template, args));
+      throw new IllegalStateException(String.format(template, arg1, arg2));
     }
   }
 }
