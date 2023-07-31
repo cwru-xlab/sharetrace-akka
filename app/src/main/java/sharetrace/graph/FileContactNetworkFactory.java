e41d3487ea931ac7d22fe77333f014e74f102d5f
Ryan <rttatton@amazon.com>
2023-07-31 09:16:44 -0400

    refactor: re-organize to use try-with-resources

--- app/src/main/java/sharetrace/graph/FileContactNetworkFactory.java
+++ app/src/main/java/sharetrace/graph/FileContactNetworkFactory.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import org.jgrapht.Graph;
@@ -20,7 +21,7 @@ public record FileContactNetworkFactory<V>(
     implements ContactNetworkFactory<V>, Timestamped {
 
   private static final String TEST_INT = "1";
-  private static final BinaryOperator<Instant> MERGER = BinaryOperator.maxBy(Instant::compareTo);
+  private static final BinaryOperator<Instant> MAX = BinaryOperator.maxBy(Instant::compareTo);
 
   @Override
   public Graph<V, TemporalEdge> newTarget() {
@@ -38,27 +39,29 @@ public record FileContactNetworkFactory<V>(
   }
 
   private void generateGraph(Graph<V, TemporalEdge> target, Map<String, V> resultMap) {
-    var maxTimestamp = parseNetwork(target);
-    adjustTimestamps(target, maxTimestamp);
+    var max = parseNetwork(target);
+    adjustTimestamps(target, max);
   }
 
   private Instant parseNetwork(Graph<V, TemporalEdge> target) {
-    try (var reader = Files.newBufferedReader(path)) {
-      Iterable<String> edges = reader.lines()::iterator;
-      var maxTimestamp = Instant.MIN;
-      for (var edge : edges) {
-        var args = edge.split(delimiter);
-        var v1 = nodeParser.apply(args[1]);
-        var v2 = nodeParser.apply(args[2]);
-        if (!v1.equals(v2)) {
-          var timestamp = parseTimestamp(args[2]);
-          mergeWithNodes(target, v1, v2, timestamp);
-          maxTimestamp = MERGER.apply(maxTimestamp, timestamp);
-        }
-      }
-      return maxTimestamp;
+    var max = new AtomicReference<>(Instant.MIN);
+    try (var edges = Files.lines(path)) {
+      edges.forEach(edge -> processEdge(edge, target, max));
     } catch (IOException exception) {
       throw new UncheckedIOException(exception);
+    }
+    return max.get();
+  }
+
+  private void processEdge(
+      String edge, Graph<V, TemporalEdge> target, AtomicReference<Instant> max) {
+    var args = edge.split(delimiter);
+    var v1 = nodeParser.apply(args[1]);
+    var v2 = nodeParser.apply(args[2]);
+    if (!v1.equals(v2)) {
+      var timestamp = parseTimestamp(args[0]);
+      mergeWithNodes(target, v1, v2, timestamp);
+      max.set(MAX.apply(max.get(), timestamp));
     }
   }
 
@@ -71,14 +74,14 @@ public record FileContactNetworkFactory<V>(
     target.addVertex(v2);
     var edge = target.getEdge(v1, v2);
     if (edge != null) {
-      edge.mergeTimestamp(timestamp, MERGER);
+      edge.mergeTimestamp(timestamp, MAX);
     } else {
       target.addEdge(v1, v2).setTimestamp(timestamp);
     }
   }
 
-  private void adjustTimestamps(Graph<V, TemporalEdge> target, Instant maxTimestamp) {
-    var offset = Duration.between(maxTimestamp, timestamp);
+  private void adjustTimestamps(Graph<V, TemporalEdge> target, Instant max) {
+    var offset = Duration.between(max, timestamp);
     for (var edge : target.edgeSet()) {
       edge.mapTimestamp(t -> t.plus(offset));
       target.setEdgeWeight(edge, edge.weight());
