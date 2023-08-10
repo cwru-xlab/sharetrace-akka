43f06615d1340eefa48c03f165ed1eef4ca6bde3
Ryan <rttatton@amazon.com>
2023-08-10 01:43:19 -0400

    refactor: improve EventRecordParser efficiency

--- lib/src/main/java/sharetrace/analysis/EventRecordParser.java
+++ lib/src/main/java/sharetrace/analysis/EventRecordParser.java
@@ -1,17 +1,22 @@
 package sharetrace.analysis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import sharetrace.analysis.model.EventRecord;
 import sharetrace.util.Parser;
 
-public record EventRecordParser(ObjectMapper mapper) implements Parser<String, EventRecord> {
+public record EventRecordParser(ObjectReader reader) implements Parser<String, EventRecord> {
+
+  public EventRecordParser(ObjectMapper mapper) {
+    this(mapper.readerFor(EventRecord.class));
+  }
 
   @Override
   public EventRecord parse(String input) {
     try {
-      return mapper.readerFor(EventRecord.class).readValue(input);
+      return reader.readValue(input);
     } catch (IOException exception) {
       throw new UncheckedIOException(exception);
     }
