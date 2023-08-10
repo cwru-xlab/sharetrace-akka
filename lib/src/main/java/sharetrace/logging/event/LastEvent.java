cf1c2965607841a04ef30fb3bd2353562c31249c
Ryan <rttatton@amazon.com>
2023-08-09 21:35:32 -0400

    fix: format code

--- lib/src/main/java/sharetrace/logging/event/LastEvent.java
+++ lib/src/main/java/sharetrace/logging/event/LastEvent.java
@@ -1,12 +1,11 @@
 package sharetrace.logging.event;
 
 import akka.actor.typed.ActorRef;
-
 import java.time.Instant;
 
 public record LastEvent(int self, Instant timestamp) implements UserEvent {
 
-    public LastEvent(ActorRef<?> self, Instant timestamp) {
-        this(UserEvent.toInt(self), timestamp);
-    }
+  public LastEvent(ActorRef<?> self, Instant timestamp) {
+    this(UserEvent.toInt(self), timestamp);
+  }
 }
