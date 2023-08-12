09ac6115b066d26fe0423b5a68e81d27d37f662a
Ryan <rttatton@amazon.com>
2023-08-11 23:44:20 -0400

    refactor: compact implementation

--- app/src/main/java/sharetrace/graph/FileContactNetworkFactory.java
+++ app/src/main/java/sharetrace/graph/FileContactNetworkFactory.java
@@ -29,18 +29,14 @@ public record FileContactNetworkFactory(Path path, String delimiter, Instant tim
   }
 
   private void generateGraph(Graph<Integer, TemporalEdge> target, Map<String, ?> resultMap) {
-    var max = parseNetwork(target);
-    adjustTimestamps(target, max);
-  }
-
-  private Instant parseNetwork(Graph<Integer, TemporalEdge> target) {
     var max = new AtomicReference<>(Instant.EPOCH);
     try (var edges = Files.lines(path)) {
       edges.forEach(edge -> processEdge(edge, target, max));
     } catch (IOException exception) {
       throw new UncheckedIOException(exception);
     }
-    return max.get();
+    var offset = Duration.between(max.get(), timestamp);
+    target.edgeSet().forEach(edge -> edge.updateTime(t -> t.plus(offset)));
   }
 
   private void processEdge(
@@ -49,20 +45,9 @@ public record FileContactNetworkFactory(Path path, String delimiter, Instant tim
     var v1 = Integer.parseInt(args[1]);
     var v2 = Integer.parseInt(args[2]);
     if (v1 != v2) {
-      var timestamp = parseTimestamp(args[0]);
+      var timestamp = Instant.ofEpochSecond(Long.parseLong(args[0]));
       Graphs.addTemporalEdge(target, v1, v2, timestamp);
-      max.set(Instants.max(max.get(), timestamp));
-    }
-  }
-
-  private Instant parseTimestamp(String timestamp) {
-    return Instant.ofEpochSecond(Long.parseLong(timestamp.strip()));
-  }
-
-  private void adjustTimestamps(Graph<Integer, TemporalEdge> target, Instant max) {
-    var offset = Duration.between(max, timestamp);
-    for (var edge : target.edgeSet()) {
-      edge.updateTime(t -> t.plus(offset));
+      max.updateAndGet(t -> Instants.max(t, timestamp));
     }
   }
 }
