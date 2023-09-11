587fc96f25e3d46125469ca741c56b6407bd52cf
rtatton <rdt17@proton.me>
2023-09-11 09:48:36 -0400

    feat: remove RiskScore checks
    RiskScore objects are created internally. Rely on factories to create
    them correctly to minimize overhead

--- lib/src/main/java/sharetrace/model/RiskScore.java
+++ lib/src/main/java/sharetrace/model/RiskScore.java
@@ -9,15 +9,9 @@ import sharetrace.util.Ranges;
 public record RiskScore(double value, Instant timestamp, Instant expiresAt)
     implements TemporalScore {
 
-  private static final Range<Double> VALUE_RANGE = Range.closed(0d, 1d);
-
-  public static final double MIN_VALUE = VALUE_RANGE.lowerEndpoint();
-  public static final double MAX_VALUE = VALUE_RANGE.upperEndpoint();
+  public static final double MIN_VALUE = 0;
+  public static final double MAX_VALUE = 1;
   public static final RiskScore MIN = new RiskScore(MIN_VALUE, Instant.EPOCH, Instant.EPOCH);
-
-  public RiskScore {
-    Ranges.check("value", value, VALUE_RANGE);
-  }
 
   public RiskScore(double value, Instant timestamp, Duration expiry) {
     this(value, timestamp, timestamp.plus(expiry));
