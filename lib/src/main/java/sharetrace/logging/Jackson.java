7a111fbd2f831ac4f3c614f22b21c00514fcfa23
Ryan <rttatton@amazon.com>
2023-11-14 19:31:17 -0500

    refactor: log timestamps as milliseconds to reduce log size

--- lib/src/main/java/sharetrace/logging/Jackson.java
+++ lib/src/main/java/sharetrace/logging/Jackson.java
@@ -1,12 +1,15 @@
 package sharetrace.logging;
 
 import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.dataformat.ion.IonFactory;
 import com.fasterxml.jackson.dataformat.ion.IonObjectMapper;
+import com.fasterxml.jackson.dataformat.ion.jsr310.IonJavaTimeModule;
 import java.time.InstantSource;
 import org.apache.commons.math3.random.RandomGenerator;
 
@@ -28,16 +31,20 @@ public final class Jackson {
 
   private static class InstanceHolder {
 
-    public static final Module SHARETRACE_MODULE = newShareTraceModule();
     public static final ObjectMapper ION_OBJECT_MAPPER = configured(new IonObjectMapper());
     public static final ObjectMapper OBJECT_MAPPER = configured(new ObjectMapper());
     public static final JsonFactory ION_JSON_FACTORY = new IonFactory(ION_OBJECT_MAPPER);
 
     private static ObjectMapper configured(ObjectMapper mapper) {
-      return mapper.findAndRegisterModules().registerModule(SHARETRACE_MODULE);
+      return mapper
+          .findAndRegisterModules()
+          .registerModule(shareTraceModule())
+          .registerModule(new IonJavaTimeModule())
+          .disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS)
+          .disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
     }
 
-    private static Module newShareTraceModule() {
+    private static Module shareTraceModule() {
       return new SimpleModule()
           .addSerializer(InstantSource.class, ToStringSerializer.instance)
           .addSerializer(RandomGenerator.class, ToClassNameSerializer.INSTANCE)
