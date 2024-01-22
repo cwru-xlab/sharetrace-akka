de4e116aef092e855335c44d45af0962c3664fe7
Ryan <rttatton@amazon.com>
2024-01-22 14:38:41 -0500

    refactor: remove unused method

--- lib/src/main/java/sharetrace/logging/event/user/UserEvent.java
+++ lib/src/main/java/sharetrace/logging/event/user/UserEvent.java
@@ -1,13 +1,8 @@
 package sharetrace.logging.event.user;
 
-import akka.actor.typed.ActorRef;
 import sharetrace.logging.event.Event;
 
 public interface UserEvent extends Event {
-
-  static int toInt(ActorRef<?> ref) {
-    return Integer.parseInt(ref.path().name());
-  }
 
   int self();
 }
