8fcca0bb16f0393548347761dbee1b49b09aef2e
Ryan <rttatton@amazon.com>
2024-02-11 13:27:02 -0500

    refactor: use Int2ReferenceOpenHashMap

--- lib/src/main/java/sharetrace/model/factory/CachedRiskScoreFactory.java
+++ lib/src/main/java/sharetrace/model/factory/CachedRiskScoreFactory.java
@@ -1,14 +1,15 @@
 package sharetrace.model.factory;
 
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
-import java.util.Map;
+import it.unimi.dsi.fastutil.ints.Int2ReferenceOpenHashMap;
 import sharetrace.model.RiskScore;
+
+import java.util.Map;
 
 public record CachedRiskScoreFactory(RiskScoreFactory scoreFactory, Map<Integer, RiskScore> cache)
     implements RiskScoreFactory {
 
   public CachedRiskScoreFactory(RiskScoreFactory scoreFactory) {
-    this(scoreFactory, new Int2ObjectOpenHashMap<>());
+    this(scoreFactory, new Int2ReferenceOpenHashMap<>());
   }
 
   @Override
