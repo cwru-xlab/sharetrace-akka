502f0666b4e09c79abced1b58d1acf8941139c18
rtatton <rdt17@proton.me>
2024-03-31 17:30:55 -0400

    feat: optimize UserEventCounts

--- lib/src/main/java/sharetrace/analysis/handler/UserEventCounts.java
+++ lib/src/main/java/sharetrace/analysis/handler/UserEventCounts.java
@@ -1,6 +1,9 @@
 package sharetrace.analysis.handler;
 
+import it.unimi.dsi.fastutil.ints.Int2ReferenceMap;
 import it.unimi.dsi.fastutil.ints.Int2ReferenceOpenHashMap;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
 import it.unimi.dsi.fastutil.objects.Reference2IntMap;
 import it.unimi.dsi.fastutil.objects.Reference2IntOpenHashMap;
 import java.util.Map;
@@ -10,23 +13,26 @@ import sharetrace.logging.event.user.UserEvent;
 
 public final class UserEventCounts implements EventHandler {
 
-  private final Map<Integer, Reference2IntMap<Class<? extends UserEvent>>> counts;
+  private final Reference2IntMap<Class<? extends UserEvent>> encoding;
+  private final Int2ReferenceMap<IntList> counts;
 
   public UserEventCounts() {
+    encoding = new Reference2IntOpenHashMap<>();
     counts = new Int2ReferenceOpenHashMap<>();
   }
 
   @Override
   public void onNext(Event event) {
     if (event instanceof UserEvent e) {
-      counts
-          .computeIfAbsent(e.self(), x -> new Reference2IntOpenHashMap<>())
-          .mergeInt(e.getClass(), 1, Integer::sum);
+      var index = encoding.computeIfAbsent(e.getClass(), x -> encoding.size());
+      var userCounts = counts.computeIfAbsent(e.self(), x -> new IntArrayList());
+      userCounts.size(index + 1);
+      userCounts.set(index, userCounts.getInt(index) + 1);
     }
   }
 
   @Override
   public void onComplete(Results results) {
-    results.withScope("user").put("events", counts);
+    results.withScope("user").withScope("events").put("counts", counts).put("encoding", encoding);
   }
 }
