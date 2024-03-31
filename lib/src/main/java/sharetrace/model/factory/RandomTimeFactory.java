4a39609e9fd0c6f4e2e8b28a7b84742acb9841b5
rtatton <rdt17@proton.me>
2024-03-31 14:58:56 -0400

    feat: add type info to TimeFactory

--- lib/src/main/java/sharetrace/model/factory/RandomTimeFactory.java
+++ lib/src/main/java/sharetrace/model/factory/RandomTimeFactory.java
@@ -1,10 +1,12 @@
 package sharetrace.model.factory;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.google.common.collect.Range;
 import sharetrace.Buildable;
 import sharetrace.model.Ranges;
 import sharetrace.model.random.DistributedRandom;
 
+@JsonTypeName("Random")
 @Buildable
 public record RandomTimeFactory(DistributedRandom distribution, long period, long referenceTime)
     implements TimeFactory {
--- lib/src/main/java/sharetrace/model/factory/TimeFactory.java
+++ lib/src/main/java/sharetrace/model/factory/TimeFactory.java
@@ -1,7 +1,10 @@
 package sharetrace.model.factory;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@FunctionalInterface
 public interface TimeFactory {
 
   @JsonIgnore
