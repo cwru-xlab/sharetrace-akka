33ed1ab69ca1646f4d408129922e95c7d9e8e0fe
rtatton <rdt17@proton.me>
2024-03-31 15:04:32 -0400

    chore: remove unused methods

--- lib/src/main/java/sharetrace/model/graph/Graphs.java
+++ lib/src/main/java/sharetrace/model/graph/Graphs.java
@@ -3,13 +3,11 @@ package sharetrace.model.graph;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 import org.jgrapht.Graph;
-import org.jgrapht.GraphTests;
 import org.jgrapht.GraphType;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultGraphType;
 import org.jgrapht.opt.graph.fastutil.FastutilMapIntVertexGraph;
 
-@SuppressWarnings("unused")
 public final class Graphs {
 
   private Graphs() {}
@@ -28,50 +26,12 @@ public final class Graphs {
     }
   }
 
-  public static Graph<Integer, DefaultEdge> copy(Graph<Integer, DefaultEdge> graph) {
-    return graph.getType().isDirected() ? copyDirected(graph) : copyUndirected(graph);
-  }
-
-  public static Graph<Integer, DefaultEdge> copyDirected(Graph<Integer, DefaultEdge> directed) {
-    return copy(GraphTests.requireDirected(directed), newDirectedGraph());
-  }
-
-  public static Graph<Integer, DefaultEdge> copyUndirected(Graph<Integer, DefaultEdge> undirected) {
-    return copy(GraphTests.requireUndirected(undirected), newUndirectedGraph());
-  }
-
   public static Graph<Integer, TemporalEdge> newTemporalGraph() {
     return newGraph(DefaultGraphType.simple().asWeighted(), TemporalEdge::new);
   }
 
   public static Graph<Integer, DefaultEdge> newDirectedGraph() {
     return newGraph(DefaultGraphType.directedSimple(), DefaultEdge::new);
-  }
-
-  public static Graph<Integer, DefaultEdge> newUndirectedGraph() {
-    return newGraph(DefaultGraphType.simple(), DefaultEdge::new);
-  }
-
-  public static Graph<Integer, DefaultEdge> asDirected(Graph<Integer, DefaultEdge> graph) {
-    if (graph.getType().isDirected()) {
-      return graph;
-    }
-    var directed = newDirectedGraph();
-    for (var edge : graph.edgeSet()) {
-      var source = graph.getEdgeSource(edge);
-      var target = graph.getEdgeTarget(edge);
-      directed.addVertex(source);
-      directed.addVertex(target);
-      directed.addEdge(source, target);
-      directed.addEdge(target, source);
-    }
-    return directed;
-  }
-
-  private static Graph<Integer, DefaultEdge> copy(
-      Graph<Integer, DefaultEdge> source, Graph<Integer, DefaultEdge> target) {
-    org.jgrapht.Graphs.addGraph(target, source);
-    return target;
   }
 
   private static <E> Graph<Integer, E> newGraph(GraphType graphType, Supplier<E> edgeFactory) {
--- lib/src/main/java/sharetrace/model/random/DistributedRandom.java
+++ lib/src/main/java/sharetrace/model/random/DistributedRandom.java
@@ -2,22 +2,13 @@ package sharetrace.model.random;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@SuppressWarnings("unused")
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @FunctionalInterface
 public interface DistributedRandom {
 
   double nextDouble();
 
-  default double nextDouble(double bound) {
-    return nextDouble() * bound;
-  }
-
-  default double nextDouble(double origin, double bound) {
-    return nextDouble(bound - origin) + origin;
-  }
-
   default long nextLong(double bound) {
-    return Math.round(nextDouble(bound));
+    return Math.round(nextDouble() * bound);
   }
 }
