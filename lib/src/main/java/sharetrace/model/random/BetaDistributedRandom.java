6f0f845317aedcbf756f7217dba9c76ad6b8413d
rtatton <rdt17@proton.me>
2024-04-01 00:35:03 -0400

    fix: fixes observed during send coefficient experiments

--- lib/src/main/java/sharetrace/model/random/BetaDistributedRandom.java
+++ lib/src/main/java/sharetrace/model/random/BetaDistributedRandom.java
@@ -1,12 +1,13 @@
 package sharetrace.model.random;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import org.apache.commons.math3.distribution.BetaDistribution;
 
 @SuppressWarnings("unused")
 @JsonTypeName("Beta")
-public record BetaDistributedRandom(BetaDistribution distribution) implements DistributedRandom {
+public record BetaDistributedRandom(@JsonIgnore BetaDistribution distribution) implements DistributedRandom {
 
   @Override
   public double nextDouble() {
