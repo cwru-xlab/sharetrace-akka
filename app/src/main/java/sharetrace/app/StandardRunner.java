8d53606b2a81e490cb8c0bb5c1c4e19ebeeec7de
Ryan <rttatton@amazon.com>
2024-04-02 22:59:30 -0400

    feat: add exploratory.template and scalability.template

---
+++ app/src/main/java/sharetrace/app/StandardRunner.java
@@ -0,0 +1,25 @@
+package sharetrace.app;
+
+import sharetrace.algorithm.RiskPropagationBuilder;
+import sharetrace.config.AppConfig;
+import sharetrace.model.Context;
+import sharetrace.model.Parameters;
+
+public record StandardRunner() implements Runner {
+
+    @Override
+    public void run(Parameters parameters, Context context) {
+        var config = AppConfig.of(parameters, context);
+        var keyFactory = config.getKeyFactory();
+        for (int i = 0; i < config.getIterations(); i++) {
+            RiskPropagationBuilder.create()
+                    .context(context)
+                    .parameters(parameters)
+                    .scoreFactory(config.getScoreFactory())
+                    .networkFactory(config.getNetworkFactory())
+                    .keyFactory(keyFactory)
+                    .build()
+                    .run(config.getRepeats());
+        }
+    }
+}
