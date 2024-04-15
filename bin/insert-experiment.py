d418eda6c992f517d82f000fa9b9631ed1fef5cd
rtatton <rdt17@proton.me>
2024-04-15 09:57:57 -0400

    refactor: avoid MongoDB collection joins

--- bin/insert-experiment.py
+++ bin/insert-experiment.py
@@ -4,38 +4,41 @@ import json
 import os
 import pathlib
 import sys
-from typing import Iterable
+from typing import Iterable, Any
 
+import humps
 from pymongo import MongoClient
 
 PathLike = str | os.PathLike[str]
 
 
 def insert_experiment(client: MongoClient, experiment: pathlib.Path):
-    db = client[experiment.name]
+    collection = client["experiments"][humps.camelize(experiment.name)]
     for dataset in _iterdir(experiment):
-        db["properties"].insert_many(_load_properties(dataset))
-        db["results"].insert_many(_load_results(dataset))
+        collection.insert_many(_load_dataset(dataset))
 
 
 def _iterdir(path: pathlib.Path) -> Iterable[pathlib.Path]:
     return (p for p in path.iterdir() if p.is_dir())
 
 
-def _load_properties(dataset: pathlib.Path) -> Iterable[dict]:
+def _load_dataset(path: pathlib.Path) -> Iterable[dict[str, Any]]:
+    results = _load_results(path)
+    for props in _load_properties(path):
+        yield {"properties": props, "results": results[props["key"]]}
+
+
+def _load_results(dataset: pathlib.Path) -> dict[str, Any]:
+    with dataset.joinpath("results.json").open() as f:
+        return json.load(f)
+
+
+def _load_properties(dataset: pathlib.Path) -> Iterable[dict[str, Any]]:
     with dataset.joinpath("properties.log").open() as f:
         for line in f:
             props = json.loads(line)
             props.update(key=props.pop("k"), dataset=dataset.name, **props.pop("p"))
             yield props
-
-
-def _load_results(dataset: pathlib.Path) -> Iterable[dict]:
-    with dataset.joinpath("results.json").open() as f:
-        results = json.load(f)
-        for key, value in results.items():
-            value.update(key=key, dataset=dataset.name)
-            yield value
 
 
 if __name__ == '__main__':
