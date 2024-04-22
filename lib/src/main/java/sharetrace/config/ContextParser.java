f6f0ce747a135f064fc3c186c385a3fae4f5c6f4
rtatton <rdt17@proton.me>
2024-04-22 11:30:13 -0400

    feat: add option to fix clock time

--- lib/src/main/java/sharetrace/config/ContextParser.java
+++ lib/src/main/java/sharetrace/config/ContextParser.java
@@ -2,6 +2,7 @@ package sharetrace.config;
 
 import com.typesafe.config.Config;
 import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
+import java.time.Instant;
 import java.time.InstantSource;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
@@ -17,33 +18,39 @@ public record ContextParser(Config contextConfig) implements ConfigParser<Contex
 
   @Override
   public Context parse(Config config) {
-    var timeFactory = getTimeFactory();
+    var timeFactory = getTimeFactory(config);
     var seed = getSeed(config);
     var logged = getLogged(config);
     return ContextBuilder.create()
         .config(contextConfig)
         .timeFactory(timeFactory)
         .seed(seed)
-        .referenceTime(getReferenceTime(config, timeFactory))
+        .referenceTime(timeFactory.getTime())
         .randomGenerator(getRandomGenerator(config, seed))
         .propertyLogger(getPropertyLogger(logged))
         .eventLogger(getEventLogger(logged))
         .build();
   }
 
-  private TimeFactory getTimeFactory() {
-    return TimeFactory.from(InstantSource.system());
+  private TimeFactory getTimeFactory(Config config) {
+    var referenceTime = getReferenceTime(config);
+    var clock = config.getString("clock");
+    return switch (clock) {
+      case "fixed" -> TimeFactory.from(InstantSource.fixed(referenceTime));
+      case "system" -> TimeFactory.from(InstantSource.system());
+      default -> throw new IllegalArgumentException(clock);
+    };
+  }
+
+  private Instant getReferenceTime(Config config) {
+    var string = config.getString("reference-time");
+    return string.equals("now") ? Instant.now() : Instant.parse(string);
   }
 
   private long getSeed(Config config) {
     var seed = config.getString("seed");
     var random = ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE);
     return seed.equals("any") ? random : Integer.parseInt(seed);
-  }
-
-  private long getReferenceTime(Config config, TimeFactory timeFactory) {
-    var string = config.getString("reference-time");
-    return string.equals("now") ? timeFactory.getTime() : timeFactory.parseTime(string);
   }
 
   private RandomGenerator getRandomGenerator(Config config, long seed) {
--- lib/src/main/java/sharetrace/model/factory/TimeFactory.java
+++ lib/src/main/java/sharetrace/model/factory/TimeFactory.java
@@ -2,7 +2,6 @@ package sharetrace.model.factory;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import java.time.Instant;
 import java.time.InstantSource;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
@@ -11,10 +10,6 @@ public interface TimeFactory {
 
   static TimeFactory from(InstantSource instantSource) {
     return instantSource::millis;
-  }
-
-  default long parseTime(String time) {
-    return Instant.parse(time).toEpochMilli();
   }
 
   @JsonIgnore
