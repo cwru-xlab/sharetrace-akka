09c6de396da592d404fa7e4bff3d687abe5f373d
rtatton <rdt17@proton.me>
2024-04-29 14:19:02 -0400

    fix: remove scale free from runtime experiments

--- bin/run-runtime-experiments.sh
+++ bin/run-runtime-experiments.sh
@@ -2,7 +2,7 @@
 
 trap "exit" INT
 
-for network in "barabasi-albert" "gnm-random" "random-regular" "scale-free" "watts-strogatz"; do
+for network in "barabasi-albert" "gnm-random" "random-regular" "watts-strogatz"; do
   # Ref: https://stackoverflow.com/a/54688673
   nohup bin/run-all.sh "runtime_${network}" > nohup-$(bin/current-time.sh).out 2>&1 < /dev/null &
   sleep 1
