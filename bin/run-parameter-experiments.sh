71f6e29279d900ac11227153b7d0d063e70105f7
Ryan <rttatton@amazon.com>
2024-05-02 16:27:17 -0400

    update parameter experiments

--- bin/run-parameter-experiments.sh
+++ bin/run-parameter-experiments.sh
@@ -4,7 +4,7 @@ trap "exit" INT
 
 bin/make-configs.py parameter
 
-for network in "barabasi-albert" "gnm-random" "random-regular" "scale-free" "watts-strogatz"; do
+for network in "barabasi-albert" "gnm-random" "random-regular" "watts-strogatz"; do
   # Ref: https://stackoverflow.com/a/54688673
   nohup bin/run-all.sh "parameter_${network}" > nohup-$(bin/current-time.sh).out 2>&1 < /dev/null &
   sleep 1
