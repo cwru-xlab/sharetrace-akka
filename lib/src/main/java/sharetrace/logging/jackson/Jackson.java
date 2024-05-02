3ab2a96bcbc938da144cc367d421da25e9db1a3e
Ryan <rttatton@amazon.com>
2024-05-01 23:10:07 -0400

    chore: remove unused serializer

--- lib/src/main/java/sharetrace/logging/jackson/Jackson.java
+++ lib/src/main/java/sharetrace/logging/jackson/Jackson.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.dataformat.ion.IonFactory;
 import com.fasterxml.jackson.dataformat.ion.IonObjectMapper;
-import java.time.InstantSource;
 import org.apache.commons.math3.random.RandomGenerator;
 
 public final class Jackson {
@@ -43,7 +41,6 @@ public final class Jackson {
 
     private static Module shareTraceModule() {
       return new SimpleModule()
-          .addSerializer(InstantSource.class, new ToStringSerializer())
           .addSerializer(RandomGenerator.class, new ToClassNameSerializer())
           .addSerializer(Class.class, new ClassSerializer())
           .addKeySerializer(Class.class, new ClassKeySerializer());
