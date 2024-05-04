38d8cbcb32f6b74d449a430bc245b389d241ffca
Ryan <rttatton@amazon.com>
2024-05-04 10:05:35 -0400

    fix: fix input to run-all.sh

--- bin/run-experiment.sh
+++ bin/run-experiment.sh
@@ -8,6 +8,6 @@ bin/make-configs.py $experimentType
 
 for network in "barabasi-albert" "gnm-random" "random-regular" "watts-strogatz"; do
   # Ref: https://stackoverflow.com/a/54688673
-  nohup bin/run-all.sh "$experimentType_{network}" > nohup-$(bin/current-time.sh).out 2>&1 < /dev/null &
+  nohup bin/run-all.sh "$experimentType_$network" > nohup-$(bin/current-time.sh).out 2>&1 < /dev/null &
   sleep 1
 done
