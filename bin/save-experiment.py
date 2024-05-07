03edc71d401164a8726f91dac91b70cc3cc78b2c
rtatton <rdt17@proton.me>
2024-05-06 21:59:37 -0400

    feat: add support for parameter experiment

--- bin/save-experiment.py
+++ bin/save-experiment.py
@@ -7,35 +7,83 @@ from typing import Iterable
 import polars as pl
 
 
-def save_experiment(path: Path, experiment_type: str) -> None:
-    datasets = _load_mapped_datasets(path, experiment_type)
+def save_experiment(path: Path, kind: str) -> None:
+    datasets = _load_mapped_datasets(path, kind)
     df = pl.DataFrame(datasets)
     df.write_parquet(path / "dataset.parquet")
 
 
-def _load_mapped_datasets(path: Path, experiment_type: str) -> Iterable[dict]:
+def _load_mapped_datasets(path: Path, kind: str) -> Iterable[dict]:
     datasets = _load_datasets(path)
-    if experiment_type == "runtime":
+    if kind == "runtime":
         return map(_to_runtime_dataset, datasets)
+    elif kind == "parameter":
+        return map(_to_parameter_dataset, datasets)
     else:
-        raise ValueError(experiment_type)
+        raise ValueError(kind)
 
 
 def _to_runtime_dataset(dataset: dict) -> dict:
+    return _common_properties(dataset) | _runtime_results(dataset)
+
+
+def _to_parameter_dataset(dataset: dict) -> dict:
+    return (
+            _common_properties(dataset)
+            | _reachability_results(dataset)
+            | _runtime_results(dataset)
+            | _updates_results(dataset)
+            | _event_counts_results(dataset)
+    )
+
+
+def _reachability_results(dataset: dict) -> dict:
+    namespace = dataset["results"]["reachability"]
+    return {
+        "n_influenced": namespace["influence"],
+        "n_influences": namespace["source"],
+        "msg_reachability": namespace["message"],
+    }
+
+
+def _event_counts_results(dataset: dict) -> dict:
+    namespace = dataset["results"]["user"]["counts"]
+    return {
+        "n_receives": namespace["ReceiveEvent"],
+        "n_updates": namespace["UpdateEvent"],
+        "n_contacts": namespace["ContactEvent"],
+    }
+
+
+def _updates_results(dataset: dict) -> dict:
+    namespace = dataset["results"]["user"]["updates"]
+    return {
+        "exposure_diffs": namespace["difference"],
+        "exposure_scores": namespace["exposure"],
+    }
+
+
+def _runtime_results(dataset: dict) -> dict:
+    namespace = dataset["results"]["runtime"]
+    return {
+        "msg_runtime": namespace["MessagePassing"],
+        "total_runtime": namespace["RiskPropagation"],
+    }
+
+
+def _common_properties(dataset: dict) -> dict:
     return {
         "id": dataset["id"],
         "key": dataset["key"],
-        "net_id": dataset["network"]["id"],
-        "net_type": dataset["networkFactory"]["type"],
+        "network_id": dataset["network"]["id"],
+        "network_type": dataset["networkFactory"]["type"],
         "nodes": dataset["network"]["nodes"],
         "edges": dataset["network"]["edges"],
-        "ctr_type": dataset["networkFactory"]["timeFactory"]["random"]["type"],
-        "svr_type": dataset["scoreFactory"]["random"]["type"],
-        "str_type": dataset["scoreFactory"]["timeFactory"]["random"]["type"],
-        "mp_runtime": dataset["results"]["runtime"]["MessagePassing"],
-        "rp_runtime": dataset["results"]["runtime"]["RiskPropagation"],
-        "tr": dataset["parameters"]["transmissionRate"],
-        "sc": dataset["parameters"]["sendCoefficient"]
+        "ct_random_type": dataset["networkFactory"]["timeFactory"]["random"]["type"],
+        "sv_random_type": dataset["scoreFactory"]["random"]["type"],
+        "st_random_type": dataset["scoreFactory"]["timeFactory"]["random"]["type"],
+        "transmission_rate": dataset["parameters"]["transmissionRate"],
+        "send_coefficient": dataset["parameters"]["sendCoefficient"]
     }
 
 
@@ -53,13 +101,8 @@ def _load_dataset(path: Path) -> Iterable[dict]:
         results = json.load(f)
     with (path / "properties.log").open() as f:
         for props in map(json.loads, f):
-            yield {
-                "id": path.name,
-                "key": (key := props["k"]),
-                "results": results[key],
-                **props["p"]
-            }
+            yield {"id": path.name, "key": (key := props["k"]), "results": results[key]} | props["p"]
 
 
 if __name__ == '__main__':
-    save_experiment(path=Path(sys.argv[1]), experiment_type=sys.argv[2])
+    save_experiment(path=Path(sys.argv[1]), kind=sys.argv[2])
