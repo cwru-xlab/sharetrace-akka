12adfb22798e4ed3676725f1acdfb51e5769e39f
Ryan <rttatton@amazon.com>
2024-05-19 16:04:24 -0400

    feat: name nohups UUIDs

--- bin/run-experiment.sh
+++ bin/run-experiment.sh
@@ -8,6 +8,5 @@ bin/make-configs.py $experimentType
 
 for network in "barabasi-albert" "gnm-random" "random-regular" "watts-strogatz"; do
   # Ref: https://stackoverflow.com/a/54688673
-  nohup bin/run-all.sh "$experimentType_$network" > nohup-$(bin/current-time.sh).out 2>&1 < /dev/null &
-  sleep 1
+  nohup bin/run-all.sh "$experimentType_$network" > nohup-$(bin/uuid.sh).out 2>&1 < /dev/null &
 done
