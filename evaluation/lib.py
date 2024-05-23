490dff3c2d04ecc791b462ef05a75e7ba69ba405
rtatton <rdt17@proton.me>
2024-05-23 04:56:40 -0400

    evaluation in-progress

--- evaluation/lib.py
+++ evaluation/lib.py
@@ -14,7 +14,7 @@ def load_runtime_dataset(name: str) -> pl.LazyFrame:
 def load_parameter_dataset(name: str) -> pl.LazyFrame:
     df = (
         load_dataset(name)
-        .with_columns(user_id=pl.int_ranges("nodes"))
+        .with_columns(pl.int_ranges("n_nodes").alias("user_id"))
         .explode(cs.by_dtype(pl.List(pl.Int64), pl.List(pl.Float64)))
     )
     return check_parameter_dataset(df)
@@ -23,18 +23,23 @@ def load_parameter_dataset(name: str) -> pl.LazyFrame:
 def check_parameter_dataset(df: DataFrame) -> DataFrame:
     invalid = (
         df.lazy()
-        # Check that the number of network nodes matches the number of network users
-        .filter(pl.col("nodes").first().ne(pl.len()).over("key", "network_id"))
-        # Check that the number of network edges matches the number of contacts.
+        .filter(pl.col("n_nodes").first().ne(pl.len()).over("key", "network_id"))
+        .collect()
+    )
+    if len(invalid):
+        raise ValueError("Invalid dataset: number of nodes != number of users")
+    invalid = (
+        df.lazy()
         .filter(
-            pl.col("edges")
+            pl.col("n_edges")
             .first()
             .ne(pl.col("n_contacts").sum().truediv(2))
             .over("key", "network_id")
-        ).collect()
+        )
+        .collect()
     )
     if len(invalid):
-        raise ValueError("Invalid dataset")
+        raise ValueError("Invalid dataset: number of edges != number of contacts")
     return df
 
 
@@ -63,89 +68,105 @@ def compute_runtime_results(df: DataFrame, network_type: str) -> NDArray:
     )
 
 
-def compute_accuracy_results(df: DataFrame) -> pl.LazyFrame:
+def eq_max(col_name: str) -> pl.Expr:
+    return pl.col(col_name).eq(pl.col(col_name).max())
+
+
+def normalized(col_name: str, by: str) -> pl.Expr:
+    num = pl.col(col_name)
+    den = num.max() if by == "max" else num.sum()
+    return num.truediv(den)
+
+
+def compute_accuracy_results(
+    df: DataFrame, by_network_type: bool = True
+) -> pl.DataFrame:
+    accuracy_key = ["send_coefficient"]
+    if by_network_type:
+        accuracy_key.insert(0, "network_type")
+    percentiles = [0, 0.01, 0.1, *range(1, 6)]
+    network_user_key = ["network_id", "user_id"]
+    parametrized_network_user_key = network_user_key + ["send_coefficient"]
     return (
         df.lazy()
-        # Use the max exposure diff of each network user to compute accuracy.
+        .filter(pl.col("exposure_diff").ne(0))
+        .filter(eq_max("exposure_diff").over(parametrized_network_user_key))
         .with_columns(
-            pl.col("exposure_diffs")
-            .max()
-            .over("network_id", "user_id", "network_type", "send_coefficient")
-            .alias("max_exposure_diff")
+            pl.col("exposure_diff")
+            .sub(pl.col("exposure_diff").max())
+            .over(network_user_key)
+            .alias("exposure_diff2")
         )
-        # Only consider network users that updated their exposure score.
-        .filter(pl.col("max_exposure_diff").ne(0))
-        # Compute the exposure diff rank for each network user over all send coefficients.
         .with_columns(
-            pl.col("max_exposure_diff")
+            *[
+                pl.col("exposure_diff2")
+                .quantile(p / 100, interpolation="midpoint")
+                .alias(f"exposure_diff2_p{p}")
+                .over(accuracy_key)
+                for p in percentiles
+            ]
+        )
+        .with_columns(
+            pl.col("exposure_diff")
             .rank("dense")
-            .over("network_id", "user_id")
-            .alias("max_exposure_diff_rank")
+            .over(network_user_key)
+            .alias("exposure_diff_rank")
         )
-        # Retain send coefficients associated with the max exposure diff of each network user.
-        .filter(
-            pl.col("max_exposure_diff_rank")
-            .eq(pl.col("max_exposure_diff_rank").max())
-            .over("network_id", "user_id")
+        .filter(eq_max("exposure_diff_rank").over(network_user_key))
+        .filter(eq_max("send_coefficient").over(network_user_key))
+        .group_by(accuracy_key)
+        .agg(
+            pl.len().alias("count"),
+            *[pl.col(f"exposure_diff2_p{p}").first() for p in percentiles]
         )
-        # Retain the max send coefficient for each network user.
-        .filter(
-            pl.col("send_coefficient")
-            .eq(pl.col("send_coefficient").max())
-            .over("network_id", "user_id")
-        )
-        # Compute count statistics of send coefficients for each network type.
-        .group_by("network_type", "send_coefficient").agg(count=pl.len())
-        # Sort for plotting and accuracy calculation.
-        .sort("network_type", "send_coefficient")
+        .sort(accuracy_key)
         # Accuracy is defined as the normalized cumulative counts of send coefficients.
         .with_columns(
-            pl.col("count")
-            .truediv(pl.col("count").sum())
+            normalized("count", by="sum")
             .cum_sum(reverse=True)
-            .over("network_type")
+            .over("network_type" if by_network_type else None)
             .alias("accuracy")
         )
+        .collect()
     )
 
 
-"""
-User metrics (p0, p25, p50, p75, p100).over('network_id', 'send_coefficient'):
-- n_influenced
-- n_influences
-- msg_reachability
-
-Network metrics:
-- n_updates
-- msg_runtime
-"""
+def compute_reachability_results(df: DataFrame) -> pl.DataFrame:
+    return df.lazy().collect()
 
 
 def compute_efficiency_results(
     df: DataFrame, min_send_coefficient: float
-) -> pl.LazyFrame:
+) -> pl.DataFrame:
     return (
         df.lazy()
-        .filter(pl.col("send_coefficient") >= min_send_coefficient)
-        .with_columns(pl.col("n_influenced").median().over("key", "network_id"))
+        .filter(pl.col("send_coefficient").ge(min_send_coefficient))
         .group_by("network_id", "network_type", "send_coefficient")
-        .agg(pl.col("n_receives").sum())
-        # .with_columns(
-        #     n_receives=(pl.col.n_receives / pl.col.n_receives.max()).over("network_id"),
-        #     **{n_influenced_p0: pl.col.n_influenced.quantile(0, "midpoint")},
-        # )
-        .filter(pl.col("send_coefficient") > min_send_coefficient)
+        .agg(pl.col("n_receives").sum(), pl.col("n_updates").sum())
+        .with_columns(
+            normalized("n_receives", by="max")
+            .over("network_id", "send_coefficient")
+            .alias("n_receives_percent"),
+            normalized("n_updates", by="max")
+            .over("network_id", "send_coefficient")
+            .alias("n_updates_percent"),
+        )
+        .filter(pl.col("send_coefficient").gt(min_send_coefficient))
         .sort("network_type", "send_coefficient")
+        .collect()
     )
 
 
-def get_efficiency_box_plot(
-    df: pl.DataFrame, abs_metric: str, rel_metric: str, group_by: str = None
-):
+def collect(df: DataFrame) -> pl.DataFrame:
+    return df.collect() if isinstance(df, pl.LazyFrame) else df
+
+
+def get_efficiency_box_plot(df: DataFrame, metric: str, group_by: str = None):
+    df = collect(df)
     box_kwargs = {"by": "send_coefficient", "groupby": group_by}
-    abs_box = df.plot.box(y=abs_metric, **box_kwargs)
-    rel_box = df.plot.box(y=rel_metric, **box_kwargs)
-    scatter = df.plot.scatter(y=abs_metric, x="send_coefficient", c="orange", size=2)
+    abs_box = df.plot.box(y=metric, **box_kwargs)
+    rel_box = df.plot.box(y=f"{metric}_percent", **box_kwargs)
+    scatter = df.plot.scatter(y=metric, x="send_coefficient", c="orange", size=2)
     scatter = scatter.opts(jitter=0.5)
     return (abs_box * rel_box * scatter).opts(multi_y=True, show_legend=False)
 
