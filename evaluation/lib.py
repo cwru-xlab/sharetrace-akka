dd660be92004e622d2d4fb8cfcba98e82d750ae1
rtatton <rdt17@proton.me>
2024-09-07 15:36:17 -0400

    Working on evaluating results

--- evaluation/lib.py
+++ evaluation/lib.py
@@ -4,8 +4,8 @@ import polars as pl
 import polars.selectors as cs
 from numpy.typing import NDArray
 
-DF = pl.DataFrame
-Expr = pl.Expr
+_DF = pl.DataFrame
+_Expr = pl.Expr
 
 _network_instance_key = ["key", "network_id"]
 
@@ -14,35 +14,11 @@ class InvalidDatasetError(Exception):
     pass
 
 
-def _first(name: str) -> Expr:
-    return pl.col(name).first()
-
-
-def _max(name: str) -> Expr:
-    return pl.col(name).max()
-
-
-def _sum(name: str) -> Expr:
-    return pl.col(name).sum()
-
-
-def _eq_max(name: str) -> Expr:
-    return pl.col(name) == _max(name)
-
-
-def _normalized(name: str, by_max: bool = False) -> Expr:
-    return pl.col(name) / (_max(name) if by_max else _sum(name))
-
-
-def _percentile(name: str, value: float, interpolation: str = "midpoint") -> Expr:
-    return pl.col(name).quantile(value / 100, interpolation=interpolation)
-
-
-def load_dataset(name: str = None, path: str = None) -> DF:
+def load_dataset(name: str = None, path: str = None) -> _DF:
     return pl.read_parquet(path or f"data/{name}/experiment.parquet")
 
 
-def process_runtime_dataset(df: DF, simple: bool = False) -> DF:
+def process_runtime_dataset(df: _DF, simple: bool = False) -> _DF:
     # Remove the "burn in" iteration for JVM class loading.
     df = df.filter(pl.col("key") != "1")
     if simple:
@@ -50,7 +26,7 @@ def process_runtime_dataset(df: DF, simple: bool = False) -> DF:
     return df
 
 
-def process_parameter_dataset(df: DF) -> DF:
+def process_parameter_dataset(df: _DF) -> _DF:
     df = _add_user_id_column(df)
     df = _explode_numeric_columns(df)
     _check_parameter_dataset_users(df)
@@ -59,8 +35,8 @@ def process_parameter_dataset(df: DF) -> DF:
 
 
 def compute_accuracy_results(
-    df: DF, parameter: str, percentiles: list[float], by_network_type: bool = False
-) -> DF:
+    df: _DF, parameter: str, percentiles: list[float], by_network_type: bool = False
+) -> _DF:
     accuracy_key = [parameter]
     if by_network_type:
         # Prefer to sort first by parameter value and then by network type.
@@ -113,13 +89,13 @@ def compute_accuracy_results(
 
 
 def compute_efficiency_results(
-    df: DF,
+    df: _DF,
     parameter: str,
     min_parameter_value: float = 0.0,
     normalize: bool = False,
     aggregate: bool = False,
     by_network_type: bool = False,
-) -> DF:
+) -> _DF:
     group_key = [parameter]
     if by_network_type:
         group_key.insert(0, "network_type")
@@ -141,33 +117,11 @@ def compute_efficiency_results(
     return result.sort(group_key)
 
 
-def get_network_types(df: DF) -> list[str]:
+def get_network_types(df: _DF) -> list[str]:
     return df.select("network_type").unique().to_series().sort().to_list()
 
 
-def _add_user_id_column(df: DF) -> DF:
-    return df.with_columns(pl.int_ranges("n_nodes").alias("user_id"))
-
-
-def _explode_numeric_columns(df: DF) -> DF:
-    return df.explode(cs.by_dtype(pl.List(pl.Int64), pl.List(pl.Float64)))
-
-
-def _check_parameter_dataset_users(df: DF) -> None:
-    invalid = df.filter((_first("n_nodes") != pl.len()).over(_network_instance_key))
-    if len(invalid) > 0:
-        raise InvalidDatasetError("Number of nodes != number of users")
-
-
-def _check_parameter_dataset_contacts(df: DF) -> None:
-    invalid = df.filter(
-        (_first("n_edges") != _sum("n_contacts") / 2).over(_network_instance_key)
-    )
-    if len(invalid) > 0:
-        raise InvalidDatasetError("Number of edges != number of contacts")
-
-
-def test_runtime_normality(df: DF, test: Callable[[NDArray], Any]) -> dict[str, Any]:
+def test_runtime_normality(df: _DF, test: Callable[[NDArray], Any]) -> dict[str, Any]:
     # noinspection PyBroadException
     def test_for_network_type(network_type):
         try:
@@ -193,3 +147,49 @@ def get_efficiency_box_plot(
     scatter = df.plot.scatter(y=metric, x=parameter, c="orange", size=2)
     scatter = scatter.opts(jitter=0.5)
     return (abs_box * rel_box * scatter).opts(multi_y=True, show_legend=False)
+
+
+def _add_user_id_column(df: _DF) -> _DF:
+    return df.with_columns(pl.int_ranges("n_nodes").alias("user_id"))
+
+
+def _explode_numeric_columns(df: _DF) -> _DF:
+    return df.explode(cs.by_dtype(pl.List(pl.Int64), pl.List(pl.Float64)))
+
+
+def _check_parameter_dataset_users(df: _DF) -> None:
+    invalid = df.filter((_first("n_nodes") != pl.len()).over(_network_instance_key))
+    if len(invalid) > 0:
+        raise InvalidDatasetError("Number of nodes != number of users")
+
+
+def _check_parameter_dataset_contacts(df: _DF) -> None:
+    invalid = df.filter(
+        (_first("n_edges") != _sum("n_contacts") / 2).over(_network_instance_key)
+    )
+    if len(invalid) > 0:
+        raise InvalidDatasetError("Number of edges != number of contacts")
+
+
+def _percentile(name: str, value: float, interpolation: str = "midpoint") -> _Expr:
+    return pl.col(name).quantile(value / 100, interpolation=interpolation)
+
+
+def _first(name: str) -> _Expr:
+    return pl.col(name).first()
+
+
+def _eq_max(name: str) -> _Expr:
+    return pl.col(name) == _max(name)
+
+
+def _normalized(name: str, by_max: bool = False) -> _Expr:
+    return pl.col(name) / (_max(name) if by_max else _sum(name))
+
+
+def _max(name: str) -> _Expr:
+    return pl.col(name).max()
+
+
+def _sum(name: str) -> _Expr:
+    return pl.col(name).sum()
