c58f2f3f95fff4f64647a9efcdd02af8cae92647
rtatton <rdt17@proton.me>
2024-09-07 15:31:07 -0400

    Working on evaluating results

--- evaluation/lib.py
+++ evaluation/lib.py
@@ -1,7 +1,5 @@
-from numbers import Real
-from typing import Self
+from typing import Any, Callable
 
-import numpy as np
 import polars as pl
 import polars.selectors as cs
 from numpy.typing import NDArray
@@ -20,22 +18,24 @@ def _first(name: str) -> Expr:
     return pl.col(name).first()
 
 
+def _max(name: str) -> Expr:
+    return pl.col(name).max()
+
+
+def _sum(name: str) -> Expr:
+    return pl.col(name).sum()
+
+
 def _eq_max(name: str) -> Expr:
-    return pl.col(name).eq(pl.col(name).max())
+    return pl.col(name) == _max(name)
 
 
 def _normalized(name: str, by_max: bool = False) -> Expr:
-    num = pl.col(name)
-    den = num.max() if by_max else num.sum()
-    return num.truediv(den)
+    return pl.col(name) / (_max(name) if by_max else _sum(name))
 
 
-def _percentile(name: str, value: Real) -> Expr:
-    return (
-        pl.col(name)
-        .quantile(value / 100, interpolation="midpoint")
-        .alias(f"{name}_p{value}")
-    )
+def _percentile(name: str, value: float, interpolation: str = "midpoint") -> Expr:
+    return pl.col(name).quantile(value / 100, interpolation=interpolation)
 
 
 def load_dataset(name: str = None, path: str = None) -> DF:
@@ -44,7 +44,7 @@ def load_dataset(name: str = None, path: str = None) -> DF:
 
 def process_runtime_dataset(df: DF, simple: bool = False) -> DF:
     # Remove the "burn in" iteration for JVM class loading.
-    df = df.filter(pl.col("key").ne("1"))
+    df = df.filter(pl.col("key") != "1")
     if simple:
         return df.select("network_type", "n_nodes", "n_edges", "msg_runtime")
     return df
@@ -58,7 +58,7 @@ def process_parameter_dataset(df: DF) -> DF:
     return df
 
 
-def accuracy_results(
+def compute_accuracy_results(
     df: DF, parameter: str, percentiles: list[float], by_network_type: bool = False
 ) -> DF:
     accuracy_key = [parameter]
@@ -68,18 +68,21 @@ def accuracy_results(
     network_user_key = ["network_id", "user_id"]
     return (
         # Only consider users whose exposure score was updated.
-        df.filter(pl.col("exposure_diff").ne(0))
+        df.filter(pl.col("exposure_diff") != 0)
         # Use the maximum change in exposure score as the baseline for accuracy.
         .filter(_eq_max("exposure_diff").over(network_user_key + [parameter]))
         # Get the accuracy across parameter values for each user.
         .with_columns(
-            (1 - pl.col("exposure_diff").max().sub(pl.col("exposure_diff")))
+            (1 - _max("exposure_diff") + pl.col("exposure_diff"))
             .over(network_user_key)
             .alias("accuracy")
         )
         # Compute accuracy per parameter value (and network type).
         .with_columns(
-            [_percentile("accuracy", p).over(accuracy_key) for p in percentiles]
+            [
+                _percentile("accuracy", p).alias(f"accuracy_p{p}").over(accuracy_key)
+                for p in percentiles
+            ]
         )
         # For each user, rank in ascending order the exposure score updates across parameter values...
         .with_columns(
@@ -96,12 +99,12 @@ def accuracy_results(
         .group_by(accuracy_key)
         .agg(
             pl.len().alias("frequency"),
-            *[pl.col(f"accuracy_p{p}").first() for p in percentiles],
+            *[_first(f"accuracy_p{p}") for p in percentiles],
         )
         .sort(accuracy_key)
         # Frequency accumulates in descending order by parameter value, so use cumulative sum.
         .with_columns(
-            _normalized("frequency", by_max=False)
+            _normalized("frequency")
             .cum_sum(reverse=True)
             .over("network_type" if by_network_type else None)
             .alias("normalized_frequency")
@@ -121,19 +124,19 @@ def compute_efficiency_results(
     if by_network_type:
         group_key.insert(0, "network_type")
     result = (
-        df.filter(pl.col(parameter).ge(min_parameter_value))
+        df.filter(pl.col(parameter) >= min_parameter_value)
         .group_by(_network_instance_key + group_key)
-        .agg(pl.col("n_updates").sum())
+        .agg(_sum("n_updates"))
     )
     if normalize:
         result = result.with_columns(
             _normalized("n_updates", by_max=True).over("network_id")
         )
-    result = result.filter(pl.col(parameter).gt(min_parameter_value))
+    result = result.filter(pl.col(parameter) > min_parameter_value)
     if aggregate:
         percentiles = [0, 10, 25, 50, 75, 90, 100]
         result = result.group_by(group_key).agg(
-            [_percentile("n_updates", p) for p in percentiles]
+            [_percentile("n_updates", p).alias(f"n_updates_p{p}") for p in percentiles]
         )
     return result.sort(group_key)
 
@@ -151,24 +154,33 @@ def _explode_numeric_columns(df: DF) -> DF:
 
 
 def _check_parameter_dataset_users(df: DF) -> None:
-    invalid = df.filter(_first("n_nodes").ne(pl.len()).over(_network_instance_key))
+    invalid = df.filter((_first("n_nodes") != pl.len()).over(_network_instance_key))
     if len(invalid) > 0:
         raise InvalidDatasetError("Number of nodes != number of users")
 
 
 def _check_parameter_dataset_contacts(df: DF) -> None:
     invalid = df.filter(
-        _first("n_edges")
-        .ne(pl.col("n_contacts").sum().truediv(2))
-        .over(_network_instance_key)
+        (_first("n_edges") != _sum("n_contacts") / 2).over(_network_instance_key)
     )
     if len(invalid) > 0:
         raise InvalidDatasetError("Number of edges != number of contacts")
 
 
-def get_message_passing_runtimes(df: pl.DataFrame, network_type: str = None) -> NDArray:
+def test_runtime_normality(df: DF, test: Callable[[NDArray], Any]) -> dict[str, Any]:
+    # noinspection PyBroadException
+    def test_for_network_type(network_type):
+        try:
+            return test(*get_runtimes(df, network_type))
+        except:
+            return test(get_runtimes(df, network_type))
+
+    return {nt: test_for_network_type(nt) for nt in get_network_types(df)}
+
+
+def get_runtimes(df: pl.DataFrame, network_type: str = None) -> NDArray:
     if network_type is not None:
-        df = df.filter(pl.col("network_type").eq(network_type))
+        df = df.filter(pl.col("network_type") == network_type)
     return df.select("msg_runtime").to_numpy().flatten()
 
 
